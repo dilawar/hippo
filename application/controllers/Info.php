d57cf7837a816d43867de30c7eabd37d66544bfa
Dilawar Singh <dilawars@ncbs.res.in>
2018-04-15 19:57:01 +0530

    quickbook is to be tested later.
    Former-commit-id: d0378def42b83843f366868003d1ceaa7d676ded
    Former-commit-id: 72f20178ccec3aebe58e096c6cdff9d2e2b86b10 [formerly 2ca80e428209579bc814bc9984de61e62db93515]
    Former-commit-id: b58dd0eb8b6fbfd82432fb24552bb20f9d2392c6
    Former-commit-id: 4bb36286a341343d7ef861290ac2faa04aac4529 [formerly 4dfcd664772a95ac30b7de442cade14945f4c9c8]
    Former-commit-id: cec60ca7bb0e28c2491ad28193ed2dcc35accafd
    Former-commit-id: 2b868fe7b53f6bb39b10b76f4e2b5466b870df87

--- application/controllers/Info.php
+++ application/controllers/Info.php
@@ -1,11 +1,22 @@
 <?php
 
+require_once BASEPATH . 'autoload.php';
+
 class Info extends CI_Controller 
 {
     public function __construct( )
     {
         parent::__construct();
-        $this->load->view( 'header' );
+
+        // Show it only if accessed from intranet or user have logged in.
+        if( ! (isIntranet( ) || isAuthenticated( ) ) )
+        {
+            echo printWarning( "To access this page, either use Intranet or log-in first" );
+            echo closePage( );
+            exit;
+        }
+
+        $this->load->view('header' );
     }
 
     public function aws( )
@@ -16,6 +27,11 @@ class Info extends CI_Controller
     public function events( )
     {
         $this->load->view('events' );
+    }
+
+    public function booking( )
+    {
+        $this->load->view('allevents');
     }
 }
 
