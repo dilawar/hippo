57b83803372117494bd7c2c9a623d69d6eeaa49e
Dilawar Singh <dilawars@ncbs.res.in>
2018-04-17 10:59:12 +0530

    Updated php script.
    Former-commit-id: fda8bbef5c21fd3b620d6eeebb9ac24a65321c6f
    Former-commit-id: ff8a75b06fa59ef95aff8e47eb5e5a18de7fadd5 [formerly ba3cdc1f84996473da4b971881ebe1e93fedb1d7]
    Former-commit-id: 61f8c50bfcd1fd2b676a122c2dd0156dae7ddff5
    Former-commit-id: 0d95f9f785fdcd0412988d0fd5cd232eaead9938 [formerly 7e73fd9c1c42961e86ee8c7aa6729be0d91b58c8]
    Former-commit-id: e2d858b6c3fdbc6e025e84fb4f0a2886e649cb72
    Former-commit-id: dc94211a7d376aacfdddabeae3b183e82395f3ac

--- application/controllers/AWS.php
+++ application/controllers/AWS.php
@@ -1,12 +1,121 @@
 <?php
 
+require_once BASEPATH. 'autoload.php';
+
 trait AWS 
 {
-    public function aws( $args = '' )
+    public function aws( string $arg = '', string $arg2 = '' )
     {
-        echo $args;
-        $this->template->load( 'header.php' );
-        $this->template->load( 'user_aws' );
+        if( strtolower($arg) == 'schedulingrequest' )
+        {
+            if( $arg2 == 'submit' )
+            {
+                // Submit the given request.
+                $_POST[ 'speaker' ] = whoAmI();
+                $login = whoAmI();
+
+                // Check if preferences are available.
+                $firstPref = __get__( $_POST, 'first_preference', '' );
+                $secondPref = __get__( $_POST, 'second_preference', '' );
+                $keys = 'id,speaker,reason,created_on';
+                $updateKeys = 'created_on,reason';
+
+                // check if dates are monday. If not assign next monday.
+                $firstPref = nextMonday( $firstPref );
+                $secondPref = nextMonday( $secondPref );
+
+                if( $firstPref )
+                {
+                    $prefDate = dbDate( $firstPref );
+                    if( strtotime( 'next monday' ) >= strtotime( $prefDate) )
+                        echo printInfo( "I can not change the past without Time Machine. 
+                        Ignoring " . humanReadableDate( $prefDate ) );
+                    else
+                    {
+                        $upcomingAWSs = getTableEntries( 
+                            'upcoming_aws', 'date', "date='$prefDate'" 
+                        );
+                        if( count( $upcomingAWSs ) == 3 )
+                            echo printInfo( "Date $prefDate is not available. Ignoring ..." );
+                        else
+                        {
+                            $keys .= ',first_preference';
+                            $updateKeys .= ',first_preference';
+                        }
+                    }
+                }
+
+                if( $secondPref )
+                {
+                    $prefDate = dbDate( $secondPref );
+                    if( strtotime( 'next monday' ) >= strtotime( $prefDate) )
+                        echo printInfo( "I can not change the past without Time Machine. 
+                        Ignoring " . humanReadableDate( $prefDate ) );
+                    else
+                    {
+                        $upcomingAWSs = getTableEntries( 
+                            'upcoming_aws', 'date', "date='$prefDate'" 
+                        );
+
+                        if( count( $upcomingAWSs ) == 3 )
+                            echo printInfo( "Date $prefDate is not available. Ignoring ..." );
+                        else
+                        {
+                            $keys .= ",second_preference";
+                            $updateKeys .= ",second_preference";
+                        }
+                    }
+                }
+
+                $updateKeys .= ',status';
+                $_POST[ 'status' ] = 'PENDING';
+                $res = insertOrUpdateTable( 'aws_scheduling_request', $keys, $updateKeys, $_POST);
+
+                if( $res )
+                    $_POST[ 'id' ] = $res[ 'id' ];
+                else
+                    $sendEmail = false;
+
+                // Create subject for email
+                $subject = "Your preferences for AWS schedule has been recieved";
+
+                $msg = "<p>Dear " . loginToText( $login ) . "</p>";
+                $msg .= "<p>Your scheduling request has been logged. </p>";
+                $msg .= arrayToVerticalTableHTML( $_POST, 'info', NULL, 'response' );
+
+                $email = getLoginEmail( $login );
+                sendHTMLEmail( $msg, $subject, $email, 'hippo@lists.ncbs.res.in' );
+            }
+            else if( $arg2 == 'delete' )
+            {
+                $table = getTableEntry( 'aws_scheduling_request', 'id', $_POST );
+                if( $table )
+                    $_POST = array_merge( $_POST, $table );
+
+                $_POST[ 'status' ] = 'CANCELLED';
+                $res = updateTable( 'aws_scheduling_request', 'id'
+                            , 'status', $_POST );
+                if( $res )
+                    $subject = "You have cancelled your AWS preference";
+                else
+                    $sendEmail = false;
+            }
+            else if( $args2 )
+            {
+                // Unknown action 2.
+                echo "Unknown action $args2";
+            }
+
+            // All action are done. Send user back to aws page.
+            $this->template->load( 'header.php' );
+            $this->template->load( 'user_aws' );
+        }
+        else
+        {
+            $this->session->set_flashdata( 'error', "Unknown action $args" );
+            $this->template->load( 'header.php' );
+            $this->template->load( 'user_aws' );
+        }
     }
 
 }
