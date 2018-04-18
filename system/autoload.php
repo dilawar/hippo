078a74ed219f1c9090ea3fb502b67f32a6607d6d
Dilawar Singh <dilawars@ncbs.res.in>
2018-04-18 19:48:54 +0530

    Tiny changes.
    Former-commit-id: 58f97001b63f277dedfecf95291bc8f0ed6f07c8
    Former-commit-id: d040d4aaeaa7a8af10cd2ede0ba681f2c30ef765 [formerly 4e0ecffd496fda48a1f1bb6eb643aa27610e633c]
    Former-commit-id: 7bb32925e8324e461c9bc6e4e5d1998ec5371356
    Former-commit-id: a7ad3a2f919ea5432190126bbb9e95b3a4b0113e [formerly d6115484f2ff0d74ac6ea60ef95b5943dba575f6]
    Former-commit-id: bc1f8ec12e64a558f3e7c64a17c26dbc66f6ea49
    Former-commit-id: 1ebdc7582174e759c32927da6e83850bc2d285f0

--- system/autoload.php
+++ system/autoload.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once BASEPATH . 'database/base.php';
+require_once BASEPATH . 'extra/display_content.php';
 require_once BASEPATH . 'extra/methods.php';
 require_once BASEPATH . 'extra/tohtml.php';
 require_once BASEPATH . 'extra/check_access_permissions.php';
+require_once BASEPATH . 'database/base.php';
 
 ?>
