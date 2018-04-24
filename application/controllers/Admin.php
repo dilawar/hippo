afbf1810da9f4f5a253937947f420f7439df5f4b
Dilawar Singh <dilawars@ncbs.res.in>
2018-04-24 11:16:30 +0530

    Admin acad is being refactored.
    Former-commit-id: d06f665f8b49ba32d22d2e7d16d5b1a24b62960f
    Former-commit-id: 9d5c2bf8f7190502f1ff69ef01e5d7374abb7357 [formerly 99c8c94b37a60e371c2ef93b42081c3054879508]
    Former-commit-id: c6d09d5601461d808e3e3e1185ecd8162e935652
    Former-commit-id: f9ade9798399c708923f565132ff1c3acc9a287f [formerly 77b28f0b514f69d40dc091921ec3fb0e7940617b]
    Former-commit-id: 7fea4439fe761ec9c074f036078be3efb2297b7b
    Former-commit-id: 89c454eed53e435109188d6d04301d5c3f9310e8

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,9 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 require_once BASEPATH.'autoload.php';
 
+require_once __DIR__.'/AdminAcad.php';
+
 
 class Admin extends CI_Controller
 {
+    use AdminAcad;
+
     function index()
     {
         $this->home();
