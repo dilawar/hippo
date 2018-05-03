83a96f76b0d293c28d8e267085cb55f0eba41fd9
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-03 10:42:10 +0530

    bookmyvenue admin is working.. more or less.
    Former-commit-id: c15d47a4657a9600491aa0d5d5e569bcf383abf5
    Former-commit-id: ca2bc19c2612f238b5b18cb2a2d5af03c7d4ab2a [formerly deb49c1c336e306fb069b87401525e05a1179a57]
    Former-commit-id: e5c8a42ec7dd3ada10f3cf1b97efee0df19f7864
    Former-commit-id: 103c0da15af1140b4eb8de783aae6fcba6ae9aac [formerly c31608beaf211521c6991b10a4c7eb154aac65c8]
    Former-commit-id: 81e36e90fd19ade969cca668550d4b5e8975d018
    Former-commit-id: 8c5e0b0cb25f17bf4265a30d37b7a342a7f8c00d

--- application/views/bookmyvenue_admin_block_venues.php
+++ application/views/bookmyvenue_admin_block_venues.php
@@ -7,18 +7,22 @@ echo userHTML( );
 
 global $dbChoices;
 
+$ref = 'adminbmv';
+if(isset($controller))
+    $ref = $controller;
+
 /* Let user select venues and select the date to block them. */
 echo ' <h1> Block venues on certain dates and times </h1> ';
 
-$venues = getVenues( );
+$venues      = getVenues( );
 $venueSelect = venuesToHTMLSelect( $venues, true );
 $classSelect = arrayToSelectList( 'class'
         , explode( ',', $dbChoices[ 'bookmyvenue_requests.class' ] )
         , array()
     );
 
-$form = ' <form action="#" method="post" accept-charset="utf-8">';
-$table = ' <table class="tasks">';
+$form   = '<form action="'. site_url("$ref/block_venue_submit") .'" method="post" accept-charset="utf-8">';
+$table  = '<table class="tasks">';
 $table .= "<tr> <td> <strong>Select one or more venues </strong> </td><td> $venueSelect </td> </tr>";
 $table .= '<tr><td>Pick dates</td>
             <td> <input type="text" name="dates" class="multidatespicker" value="" /></td></tr>';
@@ -29,69 +33,13 @@ $table .= '<tr> <td>End Time</td>
 $table .= '<tr> <td>Reason for blocking</td> <td> <input type="text" name="reason" value="" /></td></tr>';
 $table .= '<tr> <td>Type of event</td> <td>' . $classSelect .' </td></tr>';
 $table .= '</table>';
-$form .= $table;
+$form  .= $table;
 
-$form .= ' <br />  <br />';
-$form .= '<button class="submit" name="response" value="Block">Block Venues</button>';
-$form .= '</form>';
+$form  .= ' <br />  <br />';
+$form  .= '<button class="submit" name="response" value="Block">Block Venues</button>';
+$form  .= '</form>';
 echo $form;
-
-
-// Now block the venue.
-if( __get__( $_POST, 'response', '' ) == 'Block' )
-{
-    $venues = __get__( $_POST, 'venue' );
-
-    $dates = __get__( $_POST, 'dates' );
-    $dates = explode( ',', $dates );
-
-    $startTime = __get__( $_POST, 'start_time' );
-    $endTime = __get__( $_POST, 'end_time' );
-
-    $gid = intval( getUniqueFieldValue( 'bookmyvenue_requests', 'gid' ) ) + 1;
-    $rid = 0;
-    foreach( $venues as $venue )
-    {
-        foreach( $dates as $date )
-        {
-            $date = dbDate( trim( $date ) );
-            $title = __get__( $_POST, 'reason', '' );
-            $class = __get__( $_POST, 'class', 'UNKNOWN' );
-
-            if( strlen( $title ) < 8 )
-            {
-                echo printInfo( "Reason for blocking '$title' is too small.
-                    At least 8 chars are required. Ignoring ..."
-                );
-                continue;
-            }
-
-            // We create a request and immediately approve it.
-            $user = whoAmI( );
-            $data = array(
-                'gid' => $gid, 'rid' => $rid
-                , 'date' => dbDate( $date )
-                , 'start_time' => $startTime
-                , 'end_time' => $endTime
-                , 'venue' => $venue
-                , 'title' => $title
-                , 'class' => $class
-                , 'description' => 'AUTO BOOKED BY Hippo'
-                , 'created_by' => whoAmI( )
-                , 'last_modified_on' => dbDateTime( 'now' )
-            );
-
-            $res = insertIntoTable( 'bookmyvenue_requests', array_keys( $data ), $data );
-            $res = approveRequest( $gid, $rid );
-            if( $res )
-                echo printInfo( "Request $gid.$rid is approved." );
-
-            $rid ++;
-        }
-    }
-}
-
 echo ' <br /> <br />';
-echo goBackToPageLink( './bookmyvenue_admin.php', 'Go Back' );
+echo goBackToPageLink( "$ref/home", 'Go Back' );
 
 ?>
