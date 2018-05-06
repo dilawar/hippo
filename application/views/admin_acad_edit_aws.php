48dcc9d0171300a846c0a2d5190a24068642fa10
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-06 16:58:17 +0530

    Admin can update AWS now.
    Former-commit-id: ab882f4c5672080719c1b4d8bbf2a2e391a121d5
    Former-commit-id: 9e96c1d37f0c983a15fe7e62bb438626a335fb8f [formerly 159df5d57db7f5dbbb3ae3196815b26270195732]
    Former-commit-id: 2f5c0b956c3f2480afe57cb157e902b9cb940689
    Former-commit-id: 90435b4db108f5b29a0c5f9af54dc29e24c866ef [formerly 417f70500b3aa64c1107af4cec024a480de673ba]
    Former-commit-id: da13c5d8267761405c4374997c9230d00382b220
    Former-commit-id: c43fadd98cb188a3af4562ab155d09f8669204c1

--- application/views/admin_acad_edit_aws.php
+++ application/views/admin_acad_edit_aws.php
@@ -1,51 +1,16 @@
 <?php
-include_once 'header.php';
-include_once 'database.php';
-include_once 'methods.php';
-include_once 'tohtml.php';
-include_once 'check_access_permissions.php';
-
-mustHaveAnyOfTheseRoles('AWS_ADMIN' );
+require_once BASEPATH.'autoload.php';
 echo userHTML( );
 
+echo " <h1>You are updating following AWS entry</h1>";
+
 $aws = getTableEntry( 'annual_work_seminars', 'id', $_POST );
+$editable = 'is_presynopsis_seminar,title,abstract';
+echo ' <form action="'.site_url("adminacad/updateaws/submit").'" method="post"> ';
+echo dbTableToHTMLTable( 'annual_work_seminars', $aws, $editable, 'Update' );
+echo '</form>';
 
-if( $_POST['response'] == 'edit' )
-{
-    $editable = 'is_presynopsis_seminar,title,abstract';
-    echo ' <form action="#" method="post" accept-charset="utf-8"> ';
-    echo dbTableToHTMLTable( 'annual_work_seminars', $aws, $editable, 'Update' );
-    echo '</form>';
-}
-
-if( $_POST[ 'response' ] == 'Update' )
-{
-    $res = updateTable( 'annual_work_seminars', 'id', 'is_presynopsis_seminar,title,abstract', $_POST );
-    if( $res )
-    {
-        echo printInfo( "Updated successfully" );
-        echo goBack( "admin_acad.php", 1 );
-    }
-}
-
-if( $_POST[ 'response' ] == 'delete' )
-{
-    $res = deleteFromTable( 'annual_work_seminars', 'id', $_POST );
-    if( $res )
-    {
-        echo printInfo( "Successfully deleted entry" );
-        echo goBack( "admin_acad.php", 1 );
-    }
-}
-
-if( $_POST[ 'response' ] == 'DO_NOTHING' )
-{
-    echo goBack( "admin_acad.php", 0);
-}
-
-
-echo goBackToPageLink( "admin_acad.php", "Go Back" );
-
+echo goBackToPageLink( "adminacad/home", "Go Back" );
 
 ?>
 
