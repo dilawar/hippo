99b41dfb5037f311477d13eaa3a8a6232398fea3
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-12 18:02:24 +0530

    Added JC view.
    Former-commit-id: b466a533fc72632f2ef26abf902b6632e29efa05
    Former-commit-id: 541e7025eea1f1be21159e3c5103e9642dd3d02d [formerly 04ad3e33c498e6ae7c48c7d65b219a8ae0d57dfb]
    Former-commit-id: 1a575fa60f87c458d443e4b8c66b888a70c66347
    Former-commit-id: d3732e863ba2a2449a7a4b3349c65dfd03331a95 [formerly bd19fc56f9a61e8bf38c3ba5a6c5f47c1ebc3b44]
    Former-commit-id: 26f4d5ad73792d96d516e20ae35b272276445059
    Former-commit-id: 79d1a97b3f767131ee429955c7eff499a86cac67

--- application/views/header.php
+++ application/views/header.php
@@ -142,7 +142,7 @@ $(document).ready(function(){
 </script>
 
 <h1><a href="<?= site_url( 'welcome' ) ?>" >
-<img src="./../../data/hippo.svg" width="50px" alt="" /> NCBS Hippo</a></h1>
+<img src="./../../data/hippo.svg" width="50px" alt="" /> Hippo</a></h1>
 
 <div style="font-size:small" class="header">
 <table class="public_links">
