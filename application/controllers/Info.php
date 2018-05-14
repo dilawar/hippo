2071bbed246654fc35f5cec964202af30ed5e85f
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-14 18:51:02 +0530

    Fixed info page redirect problem.

--- application/controllers/Info.php
+++ application/controllers/Info.php
@@ -4,21 +4,7 @@ require_once BASEPATH . 'autoload.php';
 
 class Info extends CI_Controller 
 {
-    /*
-    public function __construct( )
-    {
-        parent::__construct();
-
-        // Show it only if accessed from intranet or user have logged in.
-        if( ! (isIntranet( ) || isAuthenticated( ) ) )
-        {
-            echo printWarning( "To access this page, either use Intranet or log-in first" );
-            echo closePage( );
-            exit;
-        }
-
-    }
-    */
+    // NOTE: Checking for permission is in pre-hook.
 
     public function loadview( $view, $data = array())
     {
