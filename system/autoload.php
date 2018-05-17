bdb04cdf046b058a824a199d7f024cb046bfc118
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-17 10:12:43 +0530

    Table can be exported now.
    Former-commit-id: 4259dfdd53532a8972b10d13829c4906384cec3c
    Former-commit-id: f586c6c37bfb8e968bc3a7b02c8f32e4b4ffe311 [formerly a58a09962cd9e78867fd1ffec81bc90f6e4b363f]
    Former-commit-id: a311268ae342b01d7baec2397f2c065ea7b63d0f
    Former-commit-id: a30ddb9089bbaa18ad5623115e7fd10d9622962e [formerly 89f2f8b4c4540f2d3f80ee0d49d51d5ea72213d8]
    Former-commit-id: d268a82d560262e66a3917d4bfdd5b69d573f234
    Former-commit-id: a5a910c53777891b0ff0c445202a2871f1dbc38f

--- system/autoload.php
+++ system/autoload.php
@@ -3,6 +3,7 @@
 require_once BASEPATH . 'extra/display_content.php';
 require_once BASEPATH . 'extra/methods.php';
 require_once BASEPATH . 'extra/tohtml.php';
+require_once BASEPATH . 'extra/fromhtml.php';
 require_once BASEPATH . 'extra/check_access_permissions.php';
 require_once BASEPATH . 'database/base.php';
 
