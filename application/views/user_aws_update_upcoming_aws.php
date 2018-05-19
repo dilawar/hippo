a13de6ff6c2717288934aab26881edbfd32344ce
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-19 14:58:24 +0530

    Some changes to user edit aws.
    Former-commit-id: 8aa9e7406041a0ec1defe0c37f1152316d1b1198
    Former-commit-id: 525ce391380aa5e485060fe9c55f97aee11cee5f [formerly c3f82af043d27131540062f3158bc11920d94d0d]
    Former-commit-id: 213c12c67d2e2f3e5b3a3b8c966131945badd858
    Former-commit-id: c1cbf67b16f2fe272c75d60cb43b77e348e8c1e0 [formerly 3cb14ea71af6d56aeb721f1387b62840a5fdcef5]
    Former-commit-id: 46130934b1424acbf755ad9bb8669a3b80c3acc5
    Former-commit-id: bf45b9f3ad9dfb7eb4120815e458797a38656522

--- application/views/user_aws_update_upcoming_aws.php
+++ application/views/user_aws_update_upcoming_aws.php
@@ -1,19 +1,12 @@
 <?php
-
-include_once 'header.php';
-include_once 'check_access_permissions.php';
-mustHaveAllOfTheseRoles( array( 'USER' ) );
-
-include_once 'database.php';
-include_once 'tohtml.php';
-
+require_once BASEPATH."autoload.php";
 echo userHTML( );
 
 echo "<h2>You are updating your upcoming AWS </h2>";
 
 echo alertUser( "If you can't find supervisors/TCM members in drop down list,
     you have to go back and add them. Visit <a class=\"clickable\" href=\"user_update_supervisors.php\">this link</a>
-    to add them."
+    to add them.", false
 );
 
 echo alertUser( '<strong>DO NOT COPY/PASTE from
@@ -22,7 +15,9 @@ echo alertUser( '<strong>DO NOT COPY/PASTE from
     application, be sure to ckick on <tt>Tools -> Source code</tt> in editor
     below to see what has been pasted. Remove as much formatting as you can.
     Ideally, you should paste plain text and format it in the editor below to
-    your heart desire.');
+    your heart desire.'
+    , false
+);
 
 echo "<br>";
 
@@ -31,12 +26,12 @@ if( $_POST[ 'response' ] == 'update' )
     $awsId = $_POST[ 'id' ];
     $aws = getUpcomingAWSById( $awsId );
 
-    echo '<form method="post" action="user_aws_update_upcoming_aws_submit.php">';
+    echo '<form method="post" action="' . site_url("user/aws/update_upcoming_aws/submit").'">';
     echo editableAWSTable( -1, $aws );
     echo '<input type="hidden", name="id" value="' . $awsId . '">';
     echo '</form>';
 }
 
-echo goBackToPageLink( 'user_aws.php', 'Go back' );
+echo goBackToPageLink( 'user/aws', 'Go back' );
 
 ?>
