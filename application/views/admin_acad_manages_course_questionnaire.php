68405ef876401c95a956656c6b4ff645d547d84a
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-20 14:05:07 +0530

    updated php file more questions.

--- application/views/admin_acad_manages_course_questionnaire.php
+++ application/views/admin_acad_manages_course_questionnaire.php
@@ -11,7 +11,12 @@ $defaults = array( 'last_modified_on' => dbDateTime( 'now' )
 $defaults['id'] = getUniqueID( 'question_bank' );
 
 echo ' <h1>Add new question</h1> ';
-echo printInfo( 'Make sure to write proper category and subcategory.' );
+
+echo printInfo( 
+   'Make sure to write proper category and subcategory. For example, course feedback question, 
+    category is <tt>Course Feedback</tt> and while the subtcategories could be <tt>Personal 
+    Qualities Of Instructors</tt>, <tt>Evaluation</tt> etc..'
+    );
 
 // Form to add a question.
 echo '<form action="'.site_url("adminacad/addquestion").'" method="post">';
