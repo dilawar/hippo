3e665b38c808a72af52293b69f140ce98124f34f
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-26 21:21:08 +0530

    Created booking forms.
    Former-commit-id: e391d37b119a400829967cbd5ff036e8b2b9b693
    Former-commit-id: e6297a0aa765268398ed8ad9724956d4f0c9c735 [formerly 471ed4af6267bbb90dee8daa5c78e6f017902ff8]
    Former-commit-id: 8f875c23ec82690bd793a5406b1e50a32edd09c0
    Former-commit-id: 0e9e20aee4a13e57275507179083142ec75ce2e9 [formerly 843680b29513afafff3156df42a163166f258952]
    Former-commit-id: 2111239c0e47f993e17cc36dabd3c726f0042d7b
    Former-commit-id: 0c65ab59cb0c7703091ac84cd5ae1ac683bc3378

--- application/views/user_browse_equipments.php
+++ application/views/user_browse_equipments.php
@@ -6,32 +6,83 @@ $ref = "user";
 if(isset($controller))
     $ref=$controller;
 
-$piOrHost = getPIOrHost( whoAmI() );
+$user = whoAmI();
+$piOrHost = getPIOrHost( $user );
 $equipments = getTableEntries( 'equipments', 'name', "status='GOOD' AND faculty_in_charge='$piOrHost'");
 
 echo '<h1>Book Equipment</h1>';
 
+echo printNote( '
+    Use either of the two forms given below. First one is to book for single time only. Form on right
+    can be used to book for multiple days. 
+    ' );
+
 $equipIDS = array_map( function($x) { return $x['id']; }, $equipments);
 $enames = array_map( function($x) { return $x['name']; }, $equipments);
-$eqiptSelect = arrayToSelectList( 'equipment_id', $equipIDS, $enames);
+$equipSelect = arrayToSelectList( 'equipment_id', $equipIDS, $enames);
 
 $editable = 'equipment_id,date,start_time,end_time,comment';
 $default = array( 'id' => getUniqueID('equipment_bookings')
                     , 'booked_by' => whoAmI() 
                     , 'created_on' => dbDateTime('now')
                     , 'modified_on' => dbDateTime('now')
-                    , 'equipment_id' => $eqiptSelect
+                    , 'equipment_id' => $equipSelect
                 );
 
+$multiBook = ' <table class="editable_book_equipments">';
+$multiBook .= '<caption>Multiple booking using repeat pattern.</caption>';
+$multiBook .= "
+    <tr>
+        <td class='db_table_fieldname'>Equipment ID</td> <td> $equipSelect </td>
+    </tr>
+    <tr>
+        <td class='db_table_fieldname'>Day pattern <br /> <small>(separated by ,)</small></td>
+        <td> <input type='text' name='day_pattern' value='' placeholder='Tue,Wed,Fri' />
+    </tr>
+    <tr>
+        <td class='db_table_fieldname'>Number of repeats</td>
+        <td> <input type='text' name='num_repeat' value='4' placeholder='4' /> </td>
+    </tr>
+    <tr>
+        <td class='db_table_fieldname'>Start Time</td>
+        <td><input class='timepicker' name='start_time' value='' />
+    </tr>
+    <tr>
+        <td class='db_table_fieldname'>End Time</td>
+        <td><input class='timepicker' name='end_time' value='' />
+    </tr>
+    <tr>
+        <td class='db_table_fieldname'>Booked By</td>
+        <td><input type='hidden' name='booked_by' value='$user' />$user</td>
+    </tr>
+    <tr>
+        <td class='db_table_fieldname'>Status</td>
+        <td><input type='hidden' name='status' value='VALID' />VALID</td>
+    </tr>
+    <tr>
+        <td class='db_table_fieldname'>Comment</td>
+        <td><input type='text' name='comment' value='' />
+    </tr>
+    <tr>
+        <td colspan='2'><button style='float:right' type='submit'>Multiple book</button> </td>
+    </tr>
+    ";
+$multiBook .= '</table>';
+
+echo '<table><tr><td>';
 echo '<form action="'. site_url( "user/book_equipment") .'" method="post" accept-charset="utf-8">';
 echo dbTableToHTMLTable( 'equipment_bookings', $default, $editable, 'Book');
 echo '</form>';
-
-
+echo '</td><td>';
+echo '<form action="'. site_url( "user/multibook_equipment") .'" method="post" accept-charset="utf-8">';
+echo $multiBook;
+echo '</form>';
+echo '</td></tr>';
+echo '</table>';
 
 echo '<h1>Available equipments</h1>';
-echo ( "Following " . count( $equipments ). " equipments are available for booking 
-    for faculty-in-charge " . mailto( $piOrHost ) 
+echo printNote( "Following " . count( $equipments ). " equipments are available for booking 
+    for faculty-in-charge " . mailto( $piOrHost )  . '.'
     );
 
 if(count($equipments) > 0)
