d5d5b52726e17a44d5f2adc559346fc1c610fe21
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-29 14:40:08 +0530

    Inventory has been modified.
    Former-commit-id: 52707500b079c6963c813e69602ab0ca301a7516
    Former-commit-id: 74cc9b158b145bf745cd2348b8ccfe737935abdb [formerly 26bb307b3b1df6d21b51ce0873a58919ab38c880]
    Former-commit-id: 54298a5eb4ccb11cb7b78d61fc23baa1bfe00810
    Former-commit-id: 3ddd32f2f4557e87684c2787970851bd9a1f456e [formerly 108d435c8984fc5a13ba24d2cfddfc29ceb88e51]
    Former-commit-id: 74466deefd255ad9b3b2135d3debd2e6e74f3ce5
    Former-commit-id: 482c3a7c0f74f87c08c73eef9d06603ced374998

--- application/views/user_inventory_manage.php
+++ application/views/user_inventory_manage.php
@@ -32,7 +32,8 @@ if( count($items) > 0)
 
 echo goBackToPageLink( "user/home", "Go Home" );
 
-echo '<h1> Add/Update inventory item</h1>';
+echo '<h1> Add or update inventory item</h1>';
+echo ' <br />';
 
 $newID = getUniqueID( 'inventory');
 $inventory = array( 'id' => $newID, 'faculty_in_charge' => $piOrHost
@@ -40,21 +41,26 @@ $inventory = array( 'id' => $newID, 'faculty_in_charge' => $piOrHost
         , 'edited_by' => whoAmI()
         );
 
+$whatToDo = 'Add';
 // If updateing the old entries, use the previous values as default parameters.
 if( __get__($_POST, 'response', '') == 'update')
     if( __get__($_POST, 'id', 0 )  > 0 )
+    {
         $inventory = getTableEntry( 'inventory', 'id', $_POST );
+        $whatToDo = 'Update';
+    }
 
-$action = 'Add';
+// Button to clear up the form.
+echo '<form action=""><button type="submit">Clear form to add new</button></form>';
 
 $editable = 'name,scientific_name,vendor,description,person_in_charge,item_condition,expiry_date';
 $editable .= ',quantity_with_unit,edited_by,requires_booking';
 
 echo '<button class="show_as_link"  id="button_show_hide"
-    value="Show" onclick="toggleShowHide( this, \'show_hide\')">Show Form</button>';
+    value="Hide" onclick="toggleShowHide( this, \'show_hide\')">Hide Form</button>';
 echo '<div id="show_hide" style="display:block">';
 echo '<form action="'.site_url("user/add_inventory_item"). '" method="post" accept-charset="utf-8">';
-echo dbTableToHTMLTable('inventory', $inventory, $editable, $action);
+echo dbTableToHTMLTable('inventory', $inventory, $editable, $whatToDo);
 echo '</form>';
 echo ' <br />';
 
