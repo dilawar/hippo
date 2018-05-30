3fd09a7779e3e9412c1a86d7a162960bc7f8fff3
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-30 19:26:34 +0530

    Added phpoffice sheet support.

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'org\\bovigo\\vfs' => array($vendorDir . '/mikey179/vfsStream/src/main/php'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Google_Service_' => array($vendorDir . '/google/apiclient-services/src'),
     'Google_' => array($vendorDir . '/google/apiclient/src'),
     'ComponentInstaller' => array($vendorDir . '/robloach/component-installer/src'),
