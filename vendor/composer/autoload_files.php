f79fcf544fb609044bbdb6aa6f6482f2d9f28c0f
Dilawar Singh <dilawars@ncbs.res.in>
2018-05-30 19:26:34 +0530

    Added phpoffice sheet support.
    Former-commit-id: 3fd09a7779e3e9412c1a86d7a162960bc7f8fff3
    Former-commit-id: 5a1741c17bb0ed3392128fc8de3db8b9ba7a08e6 [formerly 8a842471b7e222c4ff51e935a8d36771c6f8ddd1]
    Former-commit-id: e1da9f35622e2e69147136ffeaa1616aacfb1de8
    Former-commit-id: e5223c987df4a6a07a79e01c2b0585a2bb9e96a0 [formerly 35ca06d9b9fec67b7b0621ce6f2f240bb28e3c48]
    Former-commit-id: 3d25c4bf132f07dd65343b2d7177f0721b17e2b2
    Former-commit-id: 28ea7f3d1ce18dca7fb70b6f0439bb763f0ffd7d

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,10 +6,12 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    '320cde22f66dd4f5d3fd621d3e88b98f' => $vendorDir . '/symfony/polyfill-ctype/bootstrap.php',
     'a0edc8309cc5e1d60e3047b5df6b7052' => $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
     'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
     '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     'ce89ac35a6c330c55f4710717db9ff78' => $vendorDir . '/kriswallsmith/assetic/src/functions.php',
+    '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
     'decc78cc4436b1292c6c0d151b19445c' => $vendorDir . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
     '75d4b1647cdbc77a59f72bcb74df0995' => $vendorDir . '/spipu/html2pdf/html2pdf.class.php',
     '766ddebdb359eb94f1ba3ece4f768b10' => $vendorDir . '/spipu/html2pdf/_class/exception.class.php',
