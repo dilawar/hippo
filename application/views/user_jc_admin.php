51e2a9d9334707d301fc2f3f8ad0e50117e80f0b
Dilawar Singh <dilawars@ncbs.res.in>
2018-06-10 10:38:37 +0530

    Some fixes.

--- application/views/user_jc_admin.php
+++ application/views/user_jc_admin.php
@@ -280,7 +280,7 @@ echo goBackToPageLink( "user.php", "Go back" );
 
 ?>
 
-/* <!-- This should be copy pasted --> */
+<!-- This should be copy pasted -->
 <script src="<?=base_url()?>./node_modules/xlsx/dist/xlsx.core.min.js"></script>
 <script src="<?=base_url()?>./node_modules/file-saverjs/FileSaver.min.js"></script>
 <script src="<?=base_url()?>./node_modules/tableexport/dist/js/tableexport.min.js"></script>
