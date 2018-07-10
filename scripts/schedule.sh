ce20d7014df4bcdf6cfbdb90d51120d9eaabb688
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-10 19:04:27 +0530

    No +x in shell script.

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -19,7 +19,6 @@
 
 set -e
 set -o nounset                              # Treat unset variables as an error
-set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 GRAPHMLFILE="/tmp/__aws_data.graphml"
