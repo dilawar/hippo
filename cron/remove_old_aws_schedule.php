a7e92126b8eea52dd5a4369ef533f38a6d85c636
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-15 14:36:59 +0530

    Invalid or too old preferences are removed.
    Former-commit-id: e82ee7f0b1d590928b2650b513e8da36cd085965
    Former-commit-id: 8896f0159e7266acb090c2fc8a07db237e25864e [formerly 9f9301226c6104f6f89f2d7b3ef1f06ff09bb9cd]
    Former-commit-id: c5eb6402e09f2651b2a6a816ad00a619889d9558
    Former-commit-id: 3932ff3fce9c69beb15d819ac822c42c9f26d22d [formerly 01bd3f93bd55a075c96934bd6a1d04bb9f9a96c9]
    Former-commit-id: fb80625e8a3b8d5ef79e20731e792a7e459f4556
    Former-commit-id: efacc93b998d25825f9910f0278ce3e46fe1b511

--- cron/remove_old_aws_schedule.php
+++ cron/remove_old_aws_schedule.php
@@ -2,24 +2,21 @@
 
 function remove_old_aws_scheduling_prefs()
 {
-    if( trueOnGivenDayAndTime( 'this monday', '9:00' ) )
+    // Also cleanup the AWS preferences.
+    $today = dbDate( 'today' );
+    echo p("Cleaning up old requests $today." );
+    $prefs = getTableEntries( 'aws_scheduling_request'
+        , 'id'
+        , "(first_preference < '$today' AND 'second_preference' < '$today' ) AND status != 'CANCELLED'"
+    );
+
+    echo "Found total " . count( $prefs );
+
+    foreach( $prefs as $p )
     {
-        // Also cleanup the AWS preferences.
-        $today = dbDate( 'today' );
-        echo p("Cleaning up old requests $today." );
-        $prefs = getTableEntries( 'aws_scheduling_request'
-            , 'id'
-            , "first_preference < '$today' OR 'second_preference' "
-        );
-
-        echo "Found total " . count( $prefs );
-
-        foreach( $prefs as $p )
-        {
-            // Since we don't have expired field.
-            $p[ 'status' ] = 'CANCELLED';
-            updateTable( 'aws_scheduling_request', 'id', 'status', $p );
-        }
+        // Since we don't have expired field.
+        $p[ 'status' ] = 'CANCELLED';
+        updateTable( 'aws_scheduling_request', 'id', 'status', $p );
     }
 }
 
