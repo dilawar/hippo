a78d1c8ce4c3b20591408de45f4ab17870bf2f60
Dilawar Singh <dilawars@ncbs.res.in>
2018-08-05 16:11:36 +0530

    Less confusing form now.
    Former-commit-id: eb355386eff3013c2d8bf0dee89bfa5556b72ce2
    Former-commit-id: fd0fec1f2f7a9b781868fe582c319fbdd9de31ac [formerly 2fca15ca891f3ed6af8d6e712fb912d86e16cad1]
    Former-commit-id: 07d6c74a70a52d8a7920cc23af9c87d885ba2937
    Former-commit-id: 09303e80e60508bfd0d7909400bd46bbf60118ed [formerly 007bfaaa22093763716765460745ceb0359f6dfa]
    Former-commit-id: 294900f8c19a923ed7086d50f4a5dcfc52ebdbb5
    Former-commit-id: bae7ed81c3a7d5ff50aa57aa8f5cbe0fa4b1dc1a

--- tests/test_methods.php
+++ tests/test_methods.php
@@ -4,22 +4,13 @@ include_once BASEPATH.'autoload.php';
 
 function test_methods( )
 {
-    //$pat = constructRepeatPattern( "tue,wed,fri", "", "2" );
-    //echo "User pattern $pat \n";
-    //echo " My construction ";
-    //$pat = repeatPatToDays( $pat, '2017-04-11' );
-    //var_dump( $pat );
-    //print( "\nTest 2 </br> \n" );
-    $pat = constructRepeatPattern( "Mon", "", "2" );
+    $pat = constructRepeatPattern( "Mon", "All", 2 );
     print_r( $pat );
     print_r( repeatPatToDays( $pat, dbDate('today') ) );
 
-    $pat = constructRepeatPattern( "Tuesday,Wednes", "second,fourth", "6" );
-    print_r( repeatPatToDays( $pat, dbDate('today') ) );
-
-    $pat = constructRepeatPattern( "Tue", "", "2" );
-    $pat = repeatPatToDays( $pat, '2018-08-18' );
-    echo( json_encode($pat) );
+    // $pat = constructRepeatPattern( "Tuesday,Wednes", "second,fourth", 3 );
+    // print_r( $pat );
+    // print_r( repeatPatToDays( $pat, dbDate('today') ) );
 }
 
 ?>
