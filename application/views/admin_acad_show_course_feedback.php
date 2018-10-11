a5d0613a92adfc4141ea13c3851a1158e8d118a1
Dilawar Singh <dilawars@ncbs.res.in>
2018-10-11 17:03:36 +0530

    Once we have more data we need to categorize it according to coruse id.
    Former-commit-id: 43dc688c34bd89ce5750fbeac40f5c7f424d4192
    Former-commit-id: 958875fbee2f478276c7b59c2da3043d2517c757 [formerly 64dd07e53272974831bf1ea3c1ee2a7e26936240]
    Former-commit-id: 5fad80c58fcbb253274939adb800cd3aa002eab1
    Former-commit-id: 0a0a6b5cd6aa5165ee4d35d8eb38dc3f8e343511 [formerly b6cc16e6f67346a1a223d84b0f2b614e07eaaaf4]
    Former-commit-id: d2a1946685420c0288a369c2ad0e5df833740dc8
    Former-commit-id: 32f2d2ec93c6e85ce5d4ff9a9fbdc2a960a5d6cb

--- application/views/admin_acad_show_course_feedback.php
+++ application/views/admin_acad_show_course_feedback.php
@@ -9,7 +9,50 @@ if( ! isset($semester) )
 echo selectYearSemesterForm( $year, $semester );
 
 // Now get all the feedback available for this year and semester.
-$feedback = executeQuery( "SELECT * FROM poll_response WHERE status='VALID' AND external_id LIKE '%.$semester.$year'" );
-echo p("Total " . count( $feedback ) . " feedback entries are found." );
+$feedback = executeQuery( "SELECT * FROM poll_response WHERE status='VALID' 
+    AND external_id LIKE '$year.$semester.%'" 
+    );
+
+// Create a map out of feedback keys will be student id and course id.
+$feedbackMap = array();
+foreach( $feedback as $f)
+{
+    $key = $f['login'].'@@'.$f['external_id'];
+    $feedbackMap[$key][] = $f['response'];
+}
+
+echo p("Total " . count( $feedbackMap ) . " feedback entries are found." );
+
+// Get question bank.
+$questionBank = getQuestionsWithCategory( 'course feedback' );
+$questionsById = array();
+foreach( $questionBank as $cat => $qs )
+    foreach( $qs as $q )
+        $questionsById[$q['id']] = $q['question'];
+ksort($questionsById);
+
+echo '<h1> Data </h1>';
+$table = '<table class="info">';
+
+$table .= '<tr>';
+foreach( $questionsById as $qid => $qtitle )
+    $table .= "<th>$qtitle </th>";
+$table .= '</tr>';
+
+ksort( $feedbackMap );
+foreach( $feedbackMap as $key => $answers )
+{
+    $table .= "<tr>";
+    $dataInKey = explode( '@@', $key );
+    $student = $dataInKey[0];
+    $table .= "<td> $student </td>";
+    foreach( $answers as $res )
+        $table .= "<td> $res </td>";
+    $table .= "</tr>";
+}
+
+$table .= '</table>';
+echo $table;
+
 
 ?>
