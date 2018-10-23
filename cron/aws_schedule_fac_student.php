be9f02152469dfa9dbcfbb0324f8b6bfc4b79b26
Dilawar Singh <dilawars@ncbs.res.in>
2018-10-23 09:12:24 +0530

    If an email fails. Send email to list.
    Former-commit-id: 11ea5fa4fa60607db387d0a3ac1bcc55a476a785
    Former-commit-id: 9d35d1413faf3d17b4b5f0fa7b102e6726e7496f [formerly 986e4ac5e99462437813768010db1d6f6afaebe4]
    Former-commit-id: 1d95d15e9c09c2c93d8affb90baa4df0d9bf5e44
    Former-commit-id: 2e8cc5b1f4a86fa2a5b7b3f0ce8cd1e7a7a6cc94 [formerly 0560c3598bf9dc599580f33e8c9b76111f34a924]
    Former-commit-id: 029b3048ac66f993a252346069aa3fd564c0f7a2
    Former-commit-id: 6eea7f4de2b42ff3caeadbe27fa13e0280890d22

--- cron/aws_schedule_fac_student.php
+++ cron/aws_schedule_fac_student.php
@@ -3,80 +3,8 @@
 function aws_schedule_fac_student_cron( )
 {
 
-    /* --------------------------------------------------------------------------*/
-    /**
-     * @Synopsis  Send email to PI. From 10 weeks from now, a slot has been open
-     * up for his lab. He must ask her students to sign up.
-     *
-     * NOTE: If no one signs up for next two weeks, assign the student by itself.
-     * @Param 'today'
-     */
-    ///* ----------------------------------------------------------------------------*/
-    //if( trueOnGivenDayAndTime( 'this monday', '10:00' ) )
-    //{
-    //    $today = 'today';
-    //    printInfo( ' Monday and 9am. Tell faculty that their lab has been selected.' );
-    //
-    //    // Get the monday after 10 weeks.
-    //    $afterNWeeks = dbDate( strtotime( 'today' ) + 10 * 7 * 86400 );
-    //    echo printInfo( "Today is monday and after 10 weeks $afterNWeeks" );
-    //
-    //    echo humanReadableDate( $afterNWeeks );
-    //
-    //    // Get scheduled AWS on this week.
-    //    $awses = getTentativeAWSSchedule( $afterNWeeks );
-    //
-    //    // Assign these AWS and send email to speaker.
-    //    $piSlots = array( );
-    //    foreach( $awses as $aws )
-    //    {
-    //        $speaker = $aws[ 'speaker' ];
-    //        $piOrHost = getPIOrHost( $speaker );
-    //        $piSlots[ $piOrHost ][] = getLoginInfo( $speaker );
-    //        $specialization = getFacultySpecialization( $piOrHost );
-    //    }
-    //
-    //    foreach( $piSlots as $piOrHost => $speakers )
-    //    {
-    //        $numSpeakers = count( $speakers );
-    //
-    //        $numSlotExpr = "$numSpeakers slot is ";
-    //        if( count( $numSpeakers ) > 1 )
-    //            $numSlotExpr = "$numSpeakers slots are ";
-    //
-    //        $defaultAssignmentTable = arrayToHtmlTableOfLogins( $speakers );
-    //
-    //        $listSpeakers = getAWSSpeakers( 'login', "pi_or_host='$piOrHost'" );
-    //        $allSpeakersTable = arrayToHtmlTableOfLogins( $listSpeakers );
-    //
-    //        $date = $aws[ 'date' ];
-    //
-    //        $pi = findAnyoneWithEmail( $piOrHost );
-    //        $piHTML = arrayToName( $pi );
-    //
-    //
-    //        $macros = array( "FACULTY" => $piHTML
-    //            , 'AWS_DATE' => humanReadableDate( $afterNWeeks )
-    //            , 'AWS_THEME' => "'$specialization'"
-    //            , 'NUMBER_OF_SLOTS_EXPR' => $numSlotExpr
-    //            , 'DEFAULT_ASSIGNMENT_TABLE' => $defaultAssignmentTable
-    //            , 'AWS_SPEAKERS_TABLE' => $allSpeakersTable
-    //        );
-    //
-    //        $templ = emailFromTemplate(
-    //            'NOTIFY_SUPERVISOR_ABOUT_AWS_SLOT_N_WEEKS_IN_ADVANCE'
-    //            , $macros
-    //        );
-    //
-    //        // Send email.
-    //        $to = $piOrHost;
-    //        $subject = "Your lab has been assigned Annual Work Seminar slot(s) on $afterNWeeks";
-    //        sendHTMLEmail( $templ['email_body'], $subject, $to, $templ[ 'cc' ] );
-    //    }
-    //}
-
     /* 9 weeks earlier, if student fails to sign-up, select one from the list */
-    if( trueOnGivenDayAndTime( 'this monday', '11:30' ) )
+    if( trueOnGivenDayAndTime( 'this tuesday', '09:00' ) )
     {
         echo printInfo( 'Monday and 11:30am. Assign AWS' );
         $afterNWeeks = dbDate( strtotime( 'this monday' ) + 9 * 7 * 86400 );
