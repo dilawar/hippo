07bae704d0b7506e60c8128e08459dd11090705c
Dilawar Singh <dilawars@ncbs.res.in>
2018-11-24 19:32:26 +0530

    Added for publications.
    Former-commit-id: 345d8887b78fca7abe64c27c4e37d45b18ca781c
    Former-commit-id: b2a2c05d4fb939ae4db5bdf13534b5da94663bbb [formerly 661d6c3ec0f5aa3fd5fdaa6d81030407ff65c5b5]
    Former-commit-id: 18fc71f866c808e6164bdfa2aad42b13f1ab8af2
    Former-commit-id: 9661e5b5c072ce09b85617d3db8e61cdb467e589 [formerly c901ddcf5f8187e2e08503af957f634ba2aad4e2]
    Former-commit-id: 7f2342ff251b8cd815e9100febe400892335dd8d
    Former-commit-id: 2a405c71620feab27243f5a0536f410f2c89c563

---
+++ application/views/publications.php
@@ -0,0 +1,12 @@
+<?php
+include_once FCPATH.'./system/autoload.php';
+
+// This RSS url is generated from PubMed.
+$rssURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/erss.cgi?rss_guid=1RMSezfb-YLyPFjXsWq0j6qB5a_k2bOqXvlArT8q7FY1XXx1FO";
+$url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/erss.cgi?rss_guid=1NSu_CQNBizum_oQNyvEnfQmlhOTxJQa5H5sRESYexRAOfuYAI";
+$url = "http://www.ncbs.res.in/publications/export/bibtex";
+echo "getting content from $url";
+$content = file_get_contents( $url );
+echo $content;
+?>
+
