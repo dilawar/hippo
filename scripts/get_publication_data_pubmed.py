9246bb6802c8ef99849fb231d0abe9314be74ed7
Dilawar Singh <dilawars@ncbs.res.in>
2018-12-02 13:39:16 +0530

    Cool. Added script to update database.
    Former-commit-id: 4822fe633294d730e77ceb1308f8c52265f901a4
    Former-commit-id: aaf2ec94d9a7be78f67d4eda40814ec9bfccc145 [formerly a0d0fb3e2b236399ff421cae878e0856c039fb04]
    Former-commit-id: 8b8a25fa1afd69003746c73eb74bb39ed41e16dc
    Former-commit-id: 90e85b8ce5c68fcd6b6d9ae898d7639ecce9c096 [formerly e06cd5237aec7b4289302eeb7579b4c6152c8c69]
    Former-commit-id: 8517323d2223b3d8bc56431647047b41c89658d3
    Former-commit-id: 001cd23a4b9d17ba075ba1c45c7f45f1f85fcdf5

--- scripts/get_publication_data_pubmed.py
+++ scripts/get_publication_data_pubmed.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
+import os
 import requests
 import json
+import multiprocessing
+import time
+import dateparser
+from collections import defaultdict
+import hashlib
+from db_connect import db_
+cur_ = db_.cursor()
+
+def _sha512( msg ):
+    return hashlib.sha512(msg.encode('utf8')).hexdigest()
 
 url_ = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?" + \
         "db=pubmed&retmode=json&retmax=4000&usehistory=yes&" + \
@@ -12,18 +23,106 @@ def fetch_info( idlist ):
     idlist = ','.join(idlist)
     base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
     url = base + "esummary.fcgi?db=pubmed&id=%s&retmode=json" % idlist
-    return requests.get( url ).text
+    r =  requests.get( url ).text
+    time.sleep(0.5)
+    return json.loads(r)
 
-def main():
+def _make_date( x ):
+    return dateparser.parse(x)
+
+def _exeucte( cur, q ):
+    try:
+        cur.execute(q)
+    except Exception as e:
+        print('[WARN] Failed to execute: \n%s' % q )
+        print( '\tError was: %s' % e )
+        raise e
+
+def main( args ):
     global url_
     r = requests.get( url_ )
     js = json.loads( r.text )
     res = js['esearchresult']
     idlist = res['idlist']
-    for i in range(0, len(idlist), 200):
-        data = fetch_info( idlist[i:i+200] )
-        print( data )
+    # PUBMED allows upto 200 ids in one go and not more than 3 queries per
+    # second.
+    idSlices = [ idlist[i:i+200] for i in range(0, len(idlist), 200)]
+    if not os.path.exists( args.output ) or args.force: 
+        with multiprocessing.Pool(2)  as p:
+            results = p.map(fetch_info, idSlices)
+            with open( args.output, 'w' ) as f:
+                #  f.write('\n'.join(results))
+                json.dump(results,f)
+
+    # open the reuslt file.
+    with open(args.output, 'r') as f:
+        sections = json.loads( f.read() )
+
+    i = 0
+    for sec in sections:
+        data = sec['result']
+        for entry in data:
+            i+=1
+            if entry == "uids":
+                continue
+            authors = data[entry]['authors']
+            auths = ','.join([x['name'] for x in authors])
+            v = data[entry]
+            title = v['title'].replace('"', "'")
+            sha = _sha512(title)
+            date = _make_date(v['pubdate'])
+            publisher = v['fulljournalname']
+            doi = v['elocationid']
+            ptype = v['pubtype'][0] if v['pubtype'] else 'NA'
+            abstract = 'NA'
+            eid = entry
+            q = '''INSERT IGNORE INTO publications
+                    (sha512,title,abstract,publisher,type
+                    ,date,doi,urls,source,external_id
+                    ,metadata_json) 
+                VALUES
+                    ( "%s","%s","%s","%s","%s"
+                     ,"%s","%s","%s","%s","%s"
+                     ,"%s")''' % ( 
+                     sha,title,abstract,publisher,ptype
+                    , date, doi, '', 'PUBMED', eid
+                    , '' #json.dumps(data)
+                    )
+
+            try:
+                _exeucte(cur_, q)
+            except Exception as e:
+                quit()
+
+            # Update authors.
+            for auth in authors:
+                authName = auth['name']
+                affil = 'NA'
+                q = '''INSERT IGNORE INTO publication_authors 
+                    (author, affiliation, publication_title_sha, publication_title)
+                    VALUES ("%s","%s","%s","%s")''' % (authName, affil, sha, title)
+                _exeucte( cur_, q )
+
+            print( "%s\n\t%s; %s; %s" % (title, auths, date, publisher) )
+        db_.commit()
+
 
 
 if __name__ == '__main__':
-    main()
+    import argparse
+    # Argument parser.
+    description = '''Download NCBS data from PUBMED.'''
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--force', '-f'
+        , action = 'store_true',  default = False
+        , help = 'Force download even when data is downloaded before.'
+        )
+    parser.add_argument('--output', '-o'
+        , required = False, type = str
+        , default = 'pubmed.json'
+        , help = 'Output file'
+        )
+    class Args: pass 
+    args = Args()
+    parser.parse_args(namespace=args)
+    main( args )
