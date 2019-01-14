036d8e38b6853bc5c70cd152800c24da1df8b7d2
Dilawar Singh <dilawars@ncbs.res.in>
2019-01-14 15:09:29 +0530

    Time to give feedback.

--- application/views/admin_acad_show_course_feedback.php
+++ application/views/admin_acad_show_course_feedback.php
@@ -8,17 +8,43 @@ if( ! isset($semester) )
     $semester = getCurrentSemester();
 echo selectYearSemesterForm( $year, $semester );
 
+function getQuestionName( &$questionBank, $qid )
+{
+    return $questionBank[$qid];
+}
+
+function one2oneMappingWithQID( $feedback, &$questionBank )
+{
+    $response = [];
+    $a = [];
+    foreach( $feedback as $f )
+        $a[$f['question_id']] = $f['response'];
+
+    foreach( $questionBank as $qid => $q )
+        $response[$qid] = __get__( $a, $qid, 'NA' );
+
+    return $response;
+}
+
 // Now get all the feedback available for this year and semester.
 $feedback = getTableEntries( 'course_feedback_responses'
-    , 'question_id',  "status='VALID' AND year='$year' AND semester='$semester'" 
+    , 'last_modified_on',  "status='VALID' AND year='$year' AND semester='$semester'" 
 );
 
 // Create a map out of feedback keys will be student id and course id.
 $feedbackMap = array();
+$instructorMap = array( );
+$courseSpecificMap = array();
 foreach( $feedback as $f)
 {
     $key = $f['course_id'].'@@'.$f['year'] .'@@'.$f['semester'];
-    $feedbackMap[$key][] = $f;
+    $feedbackMap[$key][$f['login']][] = $f;
+
+    $inst = __get__( $f, 'instructor_email', '' );
+    if($inst)
+        $instructorMap[$inst] = $f;
+    else
+        $courseSpecificMap[$inst] = $f;
 }
 
 echo p("Total " . count( $feedbackMap ) . " feedback entries are found." );
@@ -29,6 +55,7 @@ $questionsById = array();
 foreach( $questionBank as $cat => $qs )
     foreach( $qs as $q )
         $questionsById[$q['id']] = $q['question'];
+
 ksort($questionsById);
 
 echo '<h1> Feedback Data </h1>';
@@ -37,20 +64,23 @@ ksort( $feedbackMap );
 foreach( $feedbackMap as $key => $feedbacks )
 {
     $dataInKey = explode( '@@', $key );
-
     $caption = str_replace( "@@", " ", $key);
+
     $table = '<table class="info">';
     $table .= "<caption> $caption </caption>";
 
     $table .= '<tr>';
     foreach( $questionsById as $qid => $qtitle )
-        $table .= "<th>$qtitle </th>";
+        $table .= "<th> $qid. $qtitle </th>";
     $table .= '</tr>';
 
-    foreach( $feedbacks as $fs )
+    foreach( $feedbacks as $student => $fs )
     {
         $table .= "<tr>";
-        // $table .= arrayToRowHTML( $fs, 'info' );
+        // collect by question id.
+        $row = one2oneMappingWithQID( $fs, $questionsById );
+        foreach( $row as $qid => $response )
+            $table .= '<td>'. $qid . '. ' . $response . '</td>';
         $table .= "</tr>";
     }
     $table .= '</table>';
