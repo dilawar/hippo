e7b8b113bacb368718904ab80be0cf0f9a18f8e5
Dilawar Singh <dilawars@ncbs.res.in>
2019-01-22 18:25:06 +0530

    venue assignment and aws assignment is now automatic.
    Former-commit-id: 27300f32ca116e9c388c2631ceb2dec090a777d9
    Former-commit-id: 63a9ae63ed2315ba0bd32558deacd7d9893ff98b [formerly dba4df6f2d2b473a15366ace70bdc363a2233a6a]
    Former-commit-id: d7b02ef8f225cd2ef181cda65f0bfac49e7100b2
    Former-commit-id: a94d051c3d4619617d7e70048fe7886f88aaf1ca [formerly 86ed6ab3ef6d0d5798530a55dee10129d2a2d566]
    Former-commit-id: 76d2ff4fdd15269d48913a8a3c851eacbfed5008
    Former-commit-id: f95da4a2ce9afc83df86508ea52822f6bfdad021

--- cron/aws_schedule_fac_student.php
+++ cron/aws_schedule_fac_student.php
@@ -4,9 +4,8 @@ function aws_schedule_fac_student_cron( )
 {
 
     /* 9 weeks earlier, if student fails to sign-up, select one from the list */
-    if( trueOnGivenDayAndTime( 'this tuesday', '09:00' ) )
+    if( trueOnGivenDayAndTime( 'this tuesday', '18:15' ) )
     {
-        echo printInfo( 'Monday and 11:30am. Assign AWS' );
         $afterNWeeks = dbDate( strtotime( 'this monday' ) + 9 * 7 * 86400 );
         echo printInfo( "Today is monday and after 9 weeks $afterNWeeks" );
 
@@ -15,6 +14,7 @@ function aws_schedule_fac_student_cron( )
         $awses = getTentativeAWSSchedule( $afterNWeeks );
 
         // Assign these AWS and send email to speaker.
+        $speakers = [];
         foreach( $awses as $aws )
         {
             $speaker = $aws[ 'speaker' ];
@@ -25,6 +25,7 @@ function aws_schedule_fac_student_cron( )
                 echo printInfo( "Successfully assigned $speaker for $date" );
                 $res = notifyUserAboutUpcomingAWS( $speaker, $date );
             }
+            $speakers[] = loginToText( $speaker );
         }
         rescheduleAWS( );
     }
