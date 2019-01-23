07defa1142d6226fdd04e1af3a5874b8de5b9d7b
Dilawar Singh <dilawars@ncbs.res.in>
2019-01-24 02:00:26 +0530

    Updated a bit more.
    Former-commit-id: 06cb8ac1e905f2c6e6174242803d0623a9a20701
    Former-commit-id: d86b004698b8e8891e2a3cd2c66c7ebdc084dc0d [formerly e0a26280f96e6554df0c90b68a27a91b267f6568]
    Former-commit-id: 01638b330e6f00dcc4065336c185c588201f246f
    Former-commit-id: 67dccbb95aa0d8c5f179c37542bac440aaa6e921 [formerly a472d8c1bd794657ec19e088e99a1e85cb93d44c]
    Former-commit-id: cbe514b9acdfb977b4157bdb9d4b9706f6bf4755
    Former-commit-id: 157d7952bb59a1a25bfb444c6d1b91953542ab20

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -14,13 +14,26 @@ class Ajax extends CI_Controller
 
     public function index() 
     {
+        $this->load->view("ajax_post_view");
     }
 
-    public function user_data_submit( )
+    public function user_data_submit( ) : bool
     {
-        $data = [ 'username' => $this->input->post('name') ];
-        echo json_encode( $data );
+        $fname = $this->input->post( "function" );
+        if( $fname == "isVenueAvailable" )
+        {
+            $venue = $this->input->post( "venue" );
+            $date = $this->input->post( "date" );
+            $startT = $this->input->post( "start_time" );
+            $endT = $this->input->post( "end_time" );
+            $es = getEventsOnThisVenueBetweenTime( $venue, $date, $startT, $endT);
+            $rs = getRequestsOnThisVenueBetweenTime( $venue, $date, $startT, $endT);
+            $nEvents = (count($rs) + count($es));
+            echo $nEvents;
+            return true;
+        }
     }
+
 }
 
 ?>
