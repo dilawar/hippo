f660e6aa1bf12c096aa06cd37b2ea6850870fd1b
Dilawar Singh <dilawars@ncbs.res.in>
2019-03-17 17:29:49 +0530

    All events can be queried by anyone.
    Former-commit-id: e85350b7c36035df431cb46ca0192f01c3233fdd
    Former-commit-id: 13d092ddb2cb7c9b2063b3a34be94e315c37b003 [formerly a4c5c498bd7abfa49f787d974034091076b51107]
    Former-commit-id: 1ec61bb4fc497f24261277dd9e21c09ae172a7d9
    Former-commit-id: 6ca67c2c31c5b7739dce32a8aeac9ae554b36233 [formerly 0e17235a029b7a6aa89be4beeee6f69e5475c5e6]
    Former-commit-id: 22ea163f26108e68fb1052bf4f5fbb939f8ee8da
    Former-commit-id: 6c2708a71cf70a7fb08f5e3f5fc01ac679db3228

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -18,13 +18,38 @@ class Api extends CI_Controller
         $this->send_data($what);
     }
 
+    private function send_events(string $from, string $to)
+    {
+        $events = getTableEntries( 'events', 'date'
+            , "status='VALID' AND date >= '$from' AND date < '$to'"
+            );
+        $this->send_data($events);
+    }
+
+    private function process_get_requests($args)
+    {
+        $what = $args[0];
+        if( $what == 'events' )
+        {
+            $from = __get__($args, 1, 'today');
+            $to = __get__($args, 2, strtotime("+14 days", strtotime($from)));
+            $from = dbDate($from);
+            $to = dbDate($to);
+            $this->send_events($from, $to);
+        }
+    }
+
     public function get()
     {
         $args = func_get_args();
         if(count($args) == 0)
-            $this->send_data(["Error" => "You requested nothing."]);
+        {
+            $args['status' ] = 'error';
+            $args['msg'] = "You requested nothing.";
+            $this->send_data($args);
+        }
         else
-            $this->send_data(["Hello"]);
+            $this->process_get_requests($args);
     }
 
     public function post()
