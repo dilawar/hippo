5993393a3c9e2be44298b6e694353d3291489e8a
Dilawar Singh <dilawars@ncbs.res.in>
2019-03-22 17:18:45 +0530

    Schedule only if scheduling method is random.
    Former-commit-id: cf73e112b8b662e3ae8b505c59991825d58cde63
    Former-commit-id: d73ec37c578c99f84e4ebf0070d483426af43021 [formerly 71c8d9b56f86bc8dd14d56dc8b93afa8d1e41d8b]
    Former-commit-id: dd36e2d3ba656ce49989e93069d39eb2e8d41ecc
    Former-commit-id: 144db56a975e14284f6eff3ef15930a1d39b47f1 [formerly 3e12f14133bdc68778e88a93d1f1c3b869cd6f68]
    Former-commit-id: 69a1a0ed17be368b4d4e4da88c0f63de6600f549
    Former-commit-id: 1d46fbd10873ae4ff71bab6327a447d01af0ad85

--- cron/jc_assign_n_weeks_in_advance.php
+++ cron/jc_assign_n_weeks_in_advance.php
@@ -22,39 +22,42 @@ function jc_assign_n_weeks_in_advance_cron( )
         // At JC time.
         if( isNowEqualsGivenDayAndTime( $jcDay, '10:00' ))
         {
-            echo printInfo( "JcID is $jcID with $jcDay." );
-            error_log( "Scheduling for $jcID" );
-            echo printInfo( "Scheduling for $jcID" );
+            if( $jc['scheduling_method'] == 'RANDOM')
+            {
+                echo printInfo( "JcID is $jcID with $jcDay." );
+                error_log( "Scheduling for $jcID" );
+                echo printInfo( "Scheduling for $jcID" );
 
-            // check if there is anyone scheduled on nWeeksFromjcDay
-            $schedule = getJCPresentations( $jcID, dbDate( $nWeeksFromjcDay ), '' );
-            if( $schedule && count( $schedule ) > 0 )
-            {
-                error_log( "$jcID alreay have a schedule on " . humanReadableDate( $nWeeksFromjcDay ) );
-                echo printInfo( "$jcID already have a schedule on " . humanReadableDate( $nWeeksFromjcDay) );
-                continue;
-            }
-            else
-            {
-                error_log( "Finding speaker" );
-                // Else find someone and assign.
-                $presenter = pickPresenter( $jcID );
-                if( $presenter )
+                // check if there is anyone scheduled on nWeeksFromjcDay
+                $schedule = getJCPresentations( $jcID, dbDate( $nWeeksFromjcDay ), '' );
+                if( $schedule && count( $schedule ) > 0 )
                 {
-                    $res = fixJCSchedule( $presenter
-                        , array( 'date' => dbDate( $nWeeksFromjcDay )
-                                , 'time' => dbTime( $jc['time'] )
-                                , 'jc_id' => $jcID 
-                                , 'venue' => $jc['venue']
-                            )
-                        );
-
-                    if( $res )
-                        echo printInfo( "Success! " );
+                    error_log( "$jcID alreay have a schedule on " . humanReadableDate( $nWeeksFromjcDay ) );
+                    echo printInfo( "$jcID already have a schedule on " . humanReadableDate( $nWeeksFromjcDay) );
+                    continue;
                 }
                 else
                 {
-                    echo printWarning( "Failed to find a presenter" );
+                    error_log( "Finding speaker" );
+                    // Else find someone and assign.
+                    $presenter = pickPresenter( $jcID );
+                    if( $presenter )
+                    {
+                        $res = fixJCSchedule( $presenter
+                            , array( 'date' => dbDate( $nWeeksFromjcDay )
+                                    , 'time' => dbTime( $jc['time'] )
+                                    , 'jc_id' => $jcID 
+                                    , 'venue' => $jc['venue']
+                                )
+                            );
+
+                        if( $res )
+                            echo printInfo( "Success! " );
+                    }
+                    else
+                    {
+                        echo printWarning( "Failed to find a presenter" );
+                    }
                 }
             }
         }
