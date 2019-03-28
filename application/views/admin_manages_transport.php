abf339d5cf0322d01cedd1de47425cb2806cab5e
Dilawar Singh <dilawars@ncbs.res.in>
2019-03-28 06:39:14 +0530

    Added transport table.
    Former-commit-id: bdd8623f617b00f1ae6b34422acd507fdc9c8488
    Former-commit-id: 9fc1a641444c5e90d04d209395259188e9f26b94 [formerly b7edc85ccbb7980cda9048aaca49fc4f4f69285a]
    Former-commit-id: 3db9580b2332cef2462df0d89539618cbb617461
    Former-commit-id: 096d696ee10a81975b252f69fb5ce01dab1e3159 [formerly 279fe8fafb8ec459a2b178aef67dcadea92181ea]
    Former-commit-id: dd7b6f9fa410b9f40e14c919e229f6336d568cb5
    Former-commit-id: 7883681d9e02ffbde7c76cb53664487964b4db86

--- application/views/admin_manages_transport.php
+++ application/views/admin_manages_transport.php
@@ -13,26 +13,37 @@ echo dbTableToHTMLTable( 'transport'
         , 'Add'
     );
 
-
-$tables = getTableEntries('transport', 'id', "status='VALID'");
-$hide = 'score,edited_by';
-$class = 'info sortable';
-$html = "<table id='transport' class='$class'>";
-$html .= arrayToTHRow( $tables[0], $class, $hide );
-foreach( $tables as $table )
+$tables = getTableEntries('transport', 'day', "status='VALID'");
+if($tables)
 {
-    $html .= '<tr>';
-    $html .= arrayToRowHTML( $table, $class, $hide, false, false);
-    $html .= "<td>";
-    $html .= "<form action='" .site_url('admin/delete_transport/'.$table['id']). "' method='post'>";
-    $html .= "<button type='submit'>Delete</button>";
-    $html .= "</form>";
-    $html .= "</td>";
-    $html .= '</tr>';
+    echo '<h1> Transport details </h1>';
+
+    $hide = 'id,vehicle_no,score,edited_by';
+    $class = 'info sortable exportable';
+    $html = "<table id='transport' class='$class'>";
+    $html .= arrayToTHRow( $tables[0], $class, $hide );
+    foreach( $tables as $table )
+    {
+        $html .= '<tr>';
+        $html .= arrayToRowHTML( $table, $class, $hide, false, false);
+        $html .= "<td>";
+        $html .= "<form action='" .site_url('admin/delete_transport/'.$table['id']). "' method='post'>";
+        $html .= "<button type='submit'>Delete</button>";
+        $html .= "</form>";
+        $html .= "</td>";
+        $html .= '</tr>';
+    }
+    $html .= "</table>";
+    echo $html;
 }
-$html .= "</table>";
-echo $html;
 
 echo goBackToPageLink( "admin/home", "Go back" );
 
 ?>
+
+<script src="<?=base_url()?>./node_modules/xlsx/dist/xlsx.core.min.js"></script>
+<script src="<?=base_url()?>./node_modules/file-saverjs/FileSaver.min.js"></script>
+<script src="<?=base_url()?>./node_modules/tableexport/dist/js/tableexport.min.js"></script>
+<script type="text/javascript" charset="utf-8">
+TableExport(document.getElementsByClassName("exportable"));
+</script>
