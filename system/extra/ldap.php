f05baa94fe4ffd20078f48a23479b1356ffca846
Dilawar Singh <dilawars@ncbs.res.in>
2019-04-02 22:49:07 +0530

    Fixes to venue code.
    Former-commit-id: ba9fd6b96617ecf65b239d5a2ed2c27cc0776fc3
    Former-commit-id: 15f4096912dcba62941be0e6e01e2b28a3848eae [formerly bdd6ad242f2901ca15b7ee2506f960ce7ea0380a]
    Former-commit-id: c288090208e0d79fe4ae834b759d7d4629e9d6db
    Former-commit-id: 7e2903963fada1cddddf10bc203afc4d4c15a501 [formerly 84fdef9433b12d5f85a6c4c49e3ccb7580f5c5f6]
    Former-commit-id: 5ef51dd95a0f1fe2fff041d55ba1ddf4803da2ee
    Former-commit-id: 025faa29be5889fdae8984a4ecf1ad64dbf27c59

--- system/extra/ldap.php
+++ system/extra/ldap.php
@@ -22,78 +22,102 @@ function serviceping($host, $port=389, $timeout=1)
     }
 }
 
-function getUserInfoFromLdap( $query, $ldap_ip="ldap.ncbs.res.in" ) : array
+function connectToLDAP($ldap_ip)
 {
     $ldap_ip = $ldap_ip or getConfigValue( 'LDAP_QUERY_SERVER' );
     $port = getConfigValue( 'LDAP_QUERY_SERVER_PORT' );
-
-    $login = explode( "@", $query )[0];
-
     // Search on all ports.
     $info = array( 'count' => 0 );
-
     if( 0 == serviceping( $ldap_ip, $port, 2 ) )
     {
         error_log( "Could not connect to $ldap_ip : $port . Timeout ... " );
-        return array();
+        return null;
     }
-
     $ds = @ldap_connect($ldap_ip, $port );
     @ldap_set_option( $ds, LDAP_OPT_TIMELIMIT, 1 );
     $r = @ldap_bind($ds);
-
-    if( ! $r )
+    if(! $r)
     {
         echo "LDAP binding failed. TODO: Ask user to edit details ";
-        return array();
+        return null; 
     }
+    return $ds;
+}
 
+function getUserInfoFromLdap($query, $ldap_ip="ldap.ncbs.res.in") : array
+{
+    $ds = connectToLDAP($ldap_ip);
+    if(! $ds)
+        return [];
+
+    $login = explode("@", $query)[0];
     $base_dn = "dc=ncbs,dc=res,dc=in";
     $sr = @ldap_search($ds, $base_dn , "(uid=$login)");
     $info = @ldap_get_entries($ds, $sr);
-
-    $result = array();
+    $result = [];
 
     for( $s=0; $s < $info['count']; $s++)
     {
         $i = $info[$s];
-
-        $laboffice = __get__( $i, 'profilelaboffice', array( 'NA') );
-        $joinedOn = __get__( $i, 'profiledateofjoin', array( 'NA' ) );
-
-        // We construct an array with ldap entries. Some are dumplicated with
-        // different keys to make it suitable to pass to other functions as
-        // well.
-        if( trim( $i['sn'][0] ) == 'NA' )
-            $i['sn'][0] = '';
-
-        $profileId = __get__( $i, 'profileidentification', array( -1 ) );
-        $profileidentification = $profileId[0];
-        $title = $i[ 'profilecontracttype'][0];
-        $designation = $i[ 'profiledesignation'][0];
-        $active = $i[ 'profileactive' ][0];
-
-        $result[ ] =  array( "fname" => $i['profilefirstname'][0]
-                , "first_name" => $i['profilefirstname'][0]
-                , "lname" => $i['profilelastname'][0]
-                , "last_name" => $i['profilelastname'][0]
-                , "extension" => $i['profilelandline'][0]
-                , "uid" => $profileidentification
-                , "id" => $profileidentification
-                , "email" => $i['mail'][0]
-                , "laboffice" => $laboffice[0]
-                , "joined_on" => $joinedOn[0]
-                , "title" => $title
-                , "designation" => $designation
-                , 'is_active' => $active
-            );
+        $result[ ] = pruneLDAPResponsse($i);
     }
 
-
-    if( count( $result ) > 0 )
+    // Return just one.
+    if(count($result) > 0)
         return $result[0];
-
     return array( );
+}
+
+function pruneLDAPResponsse(array $i):array
+{
+    $laboffice = __get__( $i, 'profilelaboffice', array( 'NA') );
+    $joinedOn = __get__( $i, 'profiledateofjoin', array( 'NA' ) );
+
+    // We construct an array with ldap entries. Some are dumplicated with
+    // different keys to make it suitable to pass to other functions as
+    // well.
+    if( trim( $i['sn'][0] ) == 'NA' )
+        $i['sn'][0] = '';
+
+    $profileId = __get__( $i, 'profileidentification', array( -1 ) );
+    $profileidentification = $profileId[0];
+    $title = $i[ 'profilecontracttype'][0];
+    $designation = $i[ 'profiledesignation'][0];
+    $active = $i[ 'profileactive' ][0];
+    return ["fname" => $i['profilefirstname'][0]
+        , "first_name" => $i['profilefirstname'][0]
+        , "lname" => $i['profilelastname'][0]
+        , "last_name" => $i['profilelastname'][0]
+        , "extension" => $i['profilelandline'][0]
+        , "uid" => $profileidentification
+        , "id" => $profileidentification
+        , "email" => $i['mail'][0]
+        , "laboffice" => $laboffice[0]
+        , "joined_on" => $joinedOn[0]
+        , "title" => $title
+        , "designation" => $designation
+        , 'is_active' => $active];
+}
+
+
+function getUserInfoFromLdapRelaxed($q, $ldap_ip="ldap.ncbs.res.in") : array
+{
+    $ds = connectToLDAP($ldap_ip);
+    if(! $ds)
+        return [];
+
+    $base_dn = "dc=ncbs,dc=res,dc=in";
+    $sr = @ldap_search($ds, $base_dn, 
+        "!(uid=$query*)(profilefirstname=*$q*)"
+    );
+    $info = @ldap_get_entries($ds, $sr);
+    $result = array();
+    for( $s=0; $s < $info['count']; $s++)
+    {
+        $i = $info[$s];
+        $result[] = pruneLDAPResponsse($i);
+    }
+    return $result;
 }
 
 
