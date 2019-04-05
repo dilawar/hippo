7ba83eccadfa07489e4dc23144cbfa918a76fab7
Dilawar Singh <dilawars@ncbs.res.in>
2019-04-05 14:42:20 +0530

    Deleting is proper deleting.
    Former-commit-id: 7d4fe70cc4f022a9ec5f9b6a128ad93d12d9ac05
    Former-commit-id: a4b80c8a7c2a13160f5262d1d3e1a081a45704bc [formerly 5f7c7c928575a0548ef59f95e3c3c1d4dd2f14c3]
    Former-commit-id: 7c4848687d5f6574f49ee1cc48b23aa5cebea9d6
    Former-commit-id: be876b97cee284f398a5f22174aa5a23e5505603 [formerly f626c45bcf1ca82c6093f42567b6fbb37246ada8]
    Former-commit-id: fe69674f5b40ca67255560ca233f2f1cb96842ac
    Former-commit-id: 7f15a56c6603d0493fc6232eb74d5eb99230b1da

--- application/views/admin_manages_transport.php
+++ application/views/admin_manages_transport.php
@@ -10,18 +10,18 @@ $default = [];
 $action = 'Add';
 if( __get__($_POST, 'id', 0) > 0)
 {
-    $default = getTableEntry( 'transport', 'id', $_POST);
     $action = 'Update';
+    $default = getTableEntry( 'transport', 'id', $_POST);
 }
 
 echo '<form method="post" action="'.site_url("admin/manage_transport/$action").'">';
 echo dbTableToHTMLTable( 'transport'
         , $default
-        , 'vehicle,vehicle_no,pickup_point,drop_point,day,trip_start_time,trip_end_time,comment'
+        , 'vehicle,vehicle_no,pickup_point,drop_point,day,status,trip_start_time,trip_end_time,comment'
         , $action
     );
 $groupBy = [];
-$table = getTableEntries('transport', 'day,trip_start_time', "");
+$table = getTableEntries('transport', 'day,trip_start_time', "status='VALID'");
 foreach( $table as $row )
 {
     $key = implode('||', [$row['vehicle'], $row['pickup_point'], $row['drop_point']]);
@@ -34,7 +34,7 @@ foreach( $table as $row )
 echo '<h1> Transport details </h1>';
 
 
-$hide = 'vehicle_no,edited_by,vehicle,pickup_point,drop_point,last_modified_on';
+$hide = 'vehicle_no,score,edited_by,vehicle,pickup_point,drop_point,status,last_modified_on';
 $class = 'info sortable exportable';
 foreach( $groupBy as $key => $tables )
 {
