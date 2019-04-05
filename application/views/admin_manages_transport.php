352bfe5a0b4730aadb35fc843feaf0ab31a15f6d
Dilawar Singh <dilawars@ncbs.res.in>
2019-04-05 16:39:34 +0530

    I can now add entries.
    Former-commit-id: 833e0bfce2aaea919d5120bcc1322fc84702c014
    Former-commit-id: 81b2c20979286ead5ebf787958d3753441c1b912 [formerly 286d62b4400d8f6038c31390c161ac0e7a878f4c]
    Former-commit-id: 29ae55cfa68eea1f9668a60f6450d0b1419d06a4
    Former-commit-id: fcca91ef1dfbdbc38a3ac020eba082114cfc5bd9 [formerly 5f84a5c9f2b24bcbb1fe14ecd5eaed5410cc0fa6]
    Former-commit-id: 43fd9ea45162a82a0528c4c4d2ed35e58fb1fe3c
    Former-commit-id: c0a505e86bf5eec408ca027184754e4069a97490

--- application/views/admin_manages_transport.php
+++ application/views/admin_manages_transport.php
@@ -3,70 +3,86 @@ include_once FCPATH . 'system/autoload.php';
 mustHaveAllOfTheseRoles( Array( 'ADMIN' ) );
 echo userHTML( );
 
-echo '<h3>Manage transport</h3>';
+echo '<h3>Quick Add</h3>';
 
+$table = "<table class='editable_transport' id='editable_transport'>";
+$table .= ' 
+    <tr>
+    <td>Vehicle</td> 
+    <td> <input type="text" name="vehicle" id="" value="" /> </td>
+    </tr><tr>
+    <td>Trip Pickup Point</td> 
+    <td> <input type="text" name="pickup_point" id="" value="" /> </td>
+    </tr><tr>
+    <td>Trip Drop Point</td> 
+    <td> <input type="text" name="drop_point" id="" value="" /> </td>
+    </tr><tr>
+    <td>Trip Start Times</td> 
+    <td> <input type="text" name="trip_start_times" id="" value="" 
+            placeholder="HH:MM e.g. 09:00,10:00,13:30" /> </td>
+    </tr><tr>
+    <td>Duration (mins)</td> 
+    <td> <input type="text" name="trip_duration" id="" value="" /> </td>
+    </tr><tr>
+    <td>Days</td> 
+    <td> <input type="text" name="days" id="" placeholder="Sun,Mon etc" value="" /> </td>
+    </tr><tr>
+    <td>URL/Route</td> 
+    <td> <input type="url" name="url" id="" value="" /> </td>
+    </tr>';
+$table .= "</table>";
 
-$default = [];
-$action = 'Add';
-if( __get__($_POST, 'id', 0) > 0)
-{
-    $action = 'Update';
-    $default = getTableEntry( 'transport', 'id', $_POST);
-}
+$form = '<form action="' . site_url("admin/transport/quickadd") . '" method="post">';
+$form .= $table;
+$form .= '<button class="submit" type="submit">Submit</button> ';
+$form .= '</form>';
 
-echo '<form method="post" action="'.site_url("admin/manage_transport/$action").'">';
-echo dbTableToHTMLTable( 'transport'
-        , $default
-        , 'vehicle,vehicle_no,pickup_point,drop_point,day,status,trip_start_time,trip_end_time,comment'
-        , $action
-    );
+echo "<div> $form </div>";
+echo ' <br /> <br />';
+
+// DISPLAY TRANSPORT.
 $groupBy = [];
-$table = getTableEntries('transport', 'day,trip_start_time', "status='VALID'");
-foreach( $table as $row )
+$table = getTableEntries('transport', 'pickup_point,trip_start_time,day', "status='VALID'"
+);
+foreach( $table as $r )
 {
-    $key = implode('||', [$row['vehicle'], $row['pickup_point'], $row['drop_point']]);
-    $groupBy[$key][]=$row;
+    $key = implode('||', [$r['vehicle'], $r['pickup_point'], $r['drop_point'],$r['trip_start_time']]);
+    $groupBy[$key][]=$r;
 }
 
+// Construct a new table for easy display.
+$schedule = [];
+foreach( $groupBy as $key => $tables )
+{
+    $arr = explode( '||', $key);
 
-// Group these entries by vehicle, pickup_point and drop_points.
+    // This is new key. We don't use trip_start time to group anymore.
+    $newKey = implode( '||', array_slice($arr, 0, 3));
+
+    $entry = ['days' => implode(','
+        , array_map(function($x){ return $x['day'];}, $tables)
+        )];
+    $entry['vehicle'] = $arr[0];
+    $entry['pickup_point'] = $arr[1];
+    $entry['drop_point'] = $arr[2];
+    $entry['trip_start_time'] = $arr[3];
+    $schedule[$newKey][] = $entry;
+}
+ksort($schedule);
 
 echo '<h1> Transport details </h1>';
 
-
-$hide = 'vehicle_no,score,edited_by,vehicle,pickup_point,drop_point,status,last_modified_on';
+$hide = 'vehicle_no,score,edited_by,vehicle,pickup_point,' 
+        . 'drop_point,status,last_modified_on';
 $class = 'info sortable exportable';
-foreach( $groupBy as $key => $tables )
+
+foreach( $schedule as $key => $tables )
 {
     $arr = explode('||', $key);
-    $vehicle = $arr[0];
-    $from = $arr[1];
-    $to = $arr[2];
-
-    $route ="$vehicle-$from-$to";
-    echo "<h2>$vehicle from $from to $to </h2>";
-
-    $html = "<table id='$route' class='$class'>";
-    $html .= arrayToTHRow( $tables[0], $class, $hide );
-    foreach( $tables as $table )
-    {
-        $html .= '<tr>';
-        $html .= arrayToRowHTML( $table, $class, $hide, false, false);
-        $html .= "<td>";
-        $html .= "<form action='" .site_url('admin/delete_transport/'.$table['id']). "' method='post'>";
-        $html .= "<button type='submit'>Delete</button>";
-        $html .= "</form>";
-        $html .= '</td><td>';
-        $html .= "<form action='#' method='post'>";
-        $html .= '<input type="hidden" name="id" id="" value="'.$table['id'].'" />';
-        $html .= "<button type='submit'>Edit</button>";
-        $html .= '</form>';
-        $html .= "</td>";
-        $html .= '</tr>';
-    }
-    $html .= "</table>";
-    echo $html;
+    echo "<h3>" . vsprintf( "%s from %s to %s", $arr) . '</h3>';
+    echo arraysToCombinedTableHTML($tables, 'info', 'vehicle,pickup_point,drop_point');
 }
+
 
 echo goBackToPageLink( "admin/home", "Go back" );
 
