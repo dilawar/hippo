725dbde2fd4a5356654057239c57c0a0000302be
Dilawar Singh <dilawars@ncbs.res.in>
2019-05-12 10:42:14 +0530

    bmvadmin can not see the coordinates.
    Former-commit-id: 0e7dd30a1a091c7ab4b8b168a78a4635e93314d4
    Former-commit-id: b6b97c99b9b2e63393e39a0001c31844c6294c11 [formerly 1d0ef63d1e8df8aef6437b137446af3149af9c45]
    Former-commit-id: 1e375ac287cb5e4c98dab02497b98aca4ae87e03
    Former-commit-id: 8263c77e7832265fa37767eccec12ed1840748bc [formerly a4f5551e71fd55b2bf58c2c6dd5bf46190fafc7d]
    Former-commit-id: dfe020caf2e5e77ab0ab4b6a35a50aeff00a5ed5
    Former-commit-id: 325df0c9aa3e7b4bb617487a41ba37e4cc639571

--- application/views/bookmyvenue_admin_manages_venues.php
+++ application/views/bookmyvenue_admin_manages_venues.php
@@ -1,9 +1,11 @@
 <?php 
-
 require_once BASEPATH.'autoload.php';
 echo userHTML( );
-
 $symbDelete = '<i class="fa fa-trash"></i>';
+
+// This page is accessible to both bmvadmin and admin.
+// bmvadmin can not see coordinates or the venue, that facility is only
+// available to admin. This is to reduce clutter for bmvadmin .
 
 $default = array( );
 
@@ -29,7 +31,13 @@ if( __get__( $_POST, 'response', '' ) == 'Select'  )
     $task ='update';
 }
 
-$editables = 'name,institute,building_name,floor,longitude,latitude,location,type,strength';
+$editables = 'name,institute,building_name,floor,location,type,strength';
+$hide = '';
+if($controller === 'admin')
+    $editables .= 'latitude,longitude';
+else
+    $hide .= 'latitude,longitude';
+
 $editables .= ',distance_from_ncbs';
 $editables .= ',suitable_for_conference,has_projector,has_skype';
 
@@ -39,7 +47,7 @@ if( $task == 'add new' )     // Append id to the editable since we are creating 
 {
     $editables .= ',id';
 }
-echo dbTableToHTMLTable( 'venues', $default, $editables, $task );
+echo dbTableToHTMLTable( 'venues', $default, $editables, $task, $hide);
 
 if( $task != 'add new' )
 {
@@ -48,16 +56,16 @@ if( $task != 'add new' )
 }
 
 echo '</form>';
-echo goBackToPageLink( 'adminbmv/home', 'Go back' );
+echo goBackToPageLink( "$controller/home", 'Go back' );
 
 echo '<h1> List of venues </h1>';
+echo ' <br />';
 
-echo '<table class="show_venue info">';
-
-echo arrayHeaderRow( $venues[0], 'venue' );
+echo '<table class="info sortable exportable" id="venues">';
+echo arrayHeaderRow( $venues[0], 'venue', $hide );
 foreach( $venues as $venue )
 {
-    echo arrayToRowHTML( $venue, 'venue' );
+    echo arrayToRowHTML($venue, 'venue', $hide);
     //echo '<form action="'.site_url('adminbmv/venues_action') .'" method="post">';
     //echo ' <td></td> ';
     //echo ' <td></td> ';
@@ -65,7 +73,14 @@ foreach( $venues as $venue )
 }
 echo '</table>';
 
-echo goBackToPageLink( 'adminbmv/home', 'Go back' );
+echo goBackToPageLink( "$controller/home", 'Go back' );
 
 
 ?>
+
+<script src="<?=base_url()?>./node_modules/xlsx/dist/xlsx.core.min.js"></script>
+<script src="<?=base_url()?>./node_modules/file-saverjs/FileSaver.min.js"></script>
+<script src="<?=base_url()?>./node_modules/tableexport/dist/js/tableexport.min.js"></script>
+<script type="text/javascript" charset="utf-8">
+TableExport(document.getElementsByClassName("exportable"));
+</script>
