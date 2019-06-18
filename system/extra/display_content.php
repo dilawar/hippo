1dc11705a18bbe6b0ac7fa849ad06116c79c7905
Dilawar Singh <dilawars@ncbs.res.in>
2019-06-18 16:59:45 +0530

    Log to hippo log.
    Former-commit-id: 1652c9325eba820e1fc4ac59fdef67743f7dd1e7
    Former-commit-id: 754e005bfe5ebe6adfdf9d65317f66c9c7df3188 [formerly d28d820f4e323335f4a736d6dc027b8ffa236c34]
    Former-commit-id: beebb780a7678b606cfd8cf6b605c31c384a1bfa
    Former-commit-id: 0f980d561cba9a04be8421fa535470957be34239 [formerly 7fb9cbc066691b472687f98ec20c78ffae1acf84]
    Former-commit-id: 51c0435401fa5fc5006ea936f47598c958e9b42e
    Former-commit-id: bbbe3e6c9acb7feb70c37d395fb4335222f1d7f2

--- system/extra/display_content.php
+++ system/extra/display_content.php
@@ -33,6 +33,7 @@ function flashMessage( $msg, $category = 'success' )
 function printInfo( $msg )
 {
     error_log( $msg );
+    log_message("debug", $msg );
     $info ="<div class=\"alert alert-info\">".$msg."</div>";
     return $info;
 }
