f26f472788a81960c0412712626dfe8f02ed8b5f
Dilawar Singh <dilawars@ncbs.res.in>
2019-06-20 16:51:48 +0530

    checking cron again.
    Former-commit-id: a0bd73ee5ef69055d03ddb3c392ad823e285148e
    Former-commit-id: 19f87a05a77bfb3e61cf228187a70f64e444a344 [formerly a15104560b64c5e7f1d693b6a51c4d10f92e4f9b]
    Former-commit-id: 0164b98f0eaa8c377eba3b7c90113b0618a48fa9
    Former-commit-id: 568c29690644440cbb0337fa5ed4703c46368ac4 [formerly 0ab059b689da7f23958d722b7269818d1c9a2928]
    Former-commit-id: 9de19c51ef0506162c6500120c08301093d277a3
    Former-commit-id: 90a11beef55570f4a21487b72c42f07eaaba4c53

--- hippo_cron.sh
+++ hippo_cron.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 set -x
+set -e
+STAMP=$(date)
+cat "${STAMP}: Running cron  " >> /var/log/hippo.log
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export HTTP_PROXY=http://proxy.ncbs.res.in:3128/
 export HTTPS_PROXY=http://proxy.ncbs.res.in:3128/
