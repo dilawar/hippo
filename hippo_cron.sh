178edc8641dc844a0edeab78a39929695ac04dc8
Dilawar Singh <dilawars@ncbs.res.in>
2019-06-20 17:06:03 +0530

    Fixed cron jon. Check in log next time.
    Former-commit-id: 8e8fdd93cb6ce823bf3fda7da828056ac99300b1
    Former-commit-id: 8e4bced2cf9316cede789988815871017f8656d5 [formerly ef8b7ac0ba25876b49db165e174949b83554fd36]
    Former-commit-id: c0575eb61fe9a1e385b79491565e7b310d3d4e4c
    Former-commit-id: ccbf3bee4bee3bb58ba4deadbe657a25efeba899 [formerly fe8ece87f20268d2df5ad01e4144f703e4e96706]
    Former-commit-id: af08b89bc0793e18304443fbb92e0b5ccc58479f
    Former-commit-id: 89ae22fc68ed87f139b80ed25bf6184a3381382a

--- hippo_cron.sh
+++ hippo_cron.sh
@@ -2,7 +2,7 @@
 set -x
 set -e
 STAMP=$(date)
-cat "${STAMP}: Running cron  " >> /var/log/hippo.log
+echo "${STAMP}: Running cron  " >> /var/log/hippo.log
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export HTTP_PROXY=http://proxy.ncbs.res.in:3128/
 export HTTPS_PROXY=http://proxy.ncbs.res.in:3128/
