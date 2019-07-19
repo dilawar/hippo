5c37d65eb7c1cc1377b8ee553ef184ee2a700a7d
Dilawar Singh <dilawars@ncbs.res.in>
2019-07-19 16:12:41 +0530

    Updated docker and npm dependencies.

--- application/config/config.php
+++ application/config/config.php
@@ -26,8 +26,8 @@ date_default_timezone_set('Asia/Kolkata');
 |
 */
 
-// $config['base_url'] = '';
-$config['base_url'] = 'https://ncbs.res.in/hippo';
+$config['base_url'] = getenv('HIPPO_BASE_URL');
+//$config['base_url'] = 'https://ncbs.res.in/hippo';
 
 /*
 |--------------------------------------------------------------------------
