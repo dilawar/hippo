76d168b4b97e6f1f1c74694ee4b83b58bc83b427
Dilawar Singh <dilawars@ncbs.res.in>
2019-08-21 20:02:52 +0530

    Using bootstrap.

--- application/views/user.php
+++ application/views/user.php
@@ -5,24 +5,24 @@ $thisSem = getCurrentSemester( ) . ' ' . getCurrentYear( );
 ?>
 
 <div class="row">
-    <div class="col card m-3 p-1 text-center">
-        <i class="fa fa-book fa-2x">
-            <a class="clickable" href="<?=site_url('/user/courses')?>">My Courses</a>
-        </i>
+    <div class="col card text-center m-3 p-1">
+        <a class="fa fa-book fa-2x clickable" 
+            href="<?=site_url('/user/courses')?>"> My Courses
+        </a>
         <br />
         Register/deregister courses for <?=$thisSem?> semster.
     </div>
 <?php if( __get__($cUserInfo, 'eligible_for_aws', 'NO' ) == 'YES'): ?>
     <div class="col card m-3 p-1 text-center">
-        <i class="fa fa-graduation-cap fa-2x">
-            <a class="clickable" href="<?=site_url("/user/aws")?>" >My AWS</a> 
-        </i>
-            See your previous AWSs and update them. Check
-            the details about upcoming AWS and provide preferred dates.
-            <br />
-            <a class="btn btn-secondary"
-                 href="<?=site_url("/user/update_supervisors") ?>">
-                Update TCM Members/Supervisors</a>
+        <a class="fa fa-graduation-cap clickable" 
+            href="<?=site_url("/user/aws")?>"> My AWS</a> 
+        See your previous AWSs and update them. Check
+        the details about upcoming AWS and provide preferred dates.
+        <br />
+        <a class="btn btn-secondary"
+             href="<?=site_url("/user/update_supervisors") ?>">
+            Update TCM Members/Supervisors
+        </a>
     </div>
 <?php else: ?>
     <div class="col card">
@@ -30,144 +30,146 @@ $thisSem = getCurrentSemester( ) . ' ' . getCurrentYear( );
 <?php endif; ?>
 </div>
 
+<div class="row">
+    <div class="col card text-center m-3 p-1">
+        <a class="clickable fa fa-2x" 
+            href="<?=site_url("/user/jc")?>">My Journal Clubs
+        </a>
+        <br />
+        Subscribe/Unsubscribe from journal club. See upcoming presentation.
+        Vote on presentation requests.
+    </div>
+    <div class="col card text-center m-3 p-1">
+        <a class="fa fa-2x clickable" 
+            href="<?=site_url("/user/jc_presentation_requests")?>">
+            My JC Presentation Requests
+        </a>
+        <br />
+        Submit a journal paper and a preferred date to present it. You can submit
+        one presentations requests for nay date. The community can vote on the
+        presentation requests.
+    </div>
+</div>
+
+<?php if(isJCAdmin( whoAmI())): ?>
+    <div class="row">
+        <div class="col card text-center m-3 p-1">
+            <i class="fa fa-lock fa-2x"></i>
+            <a class="clickable" href="'. site_url("/user/jcadmin"). '">JC Admin</a> <br />
+            Journal club admin</td>
+        </div>
+        <div class="col">
+        </div>
+    </div>
+<?php endif; ?>
+
+<h1>Booking</h1>
+
 <?php
-// Journal club entry.
-$table = '<table class="admin">
-    <tr>
-        <td>
-            <a class="clickable" href="'. site_url("/user/jc"). '">My Journal Clubs</a> <br />
-            Subscribe/Unsubscribe from journal club. See upcoming presentation.
-            Vote on presentation requests.
-         </td>
-        <td>
-            <a class="clickable" href="'. site_url("/user/jc_presentation_requests"). '">
-                My JC Presentation Requests</a>
-            <br />
-            Submit a journal paper and a preferred date to present it. You can submit
-            one presentations requests for nay date. The community can vote on the
-            presentation requests.
-         </td>
-    </tr>';
-
-if( isJCAdmin( whoAmI() ) )
-{
-    $table .= '<tr>
-        <td>
-        <i class="fa fa-lock fa-2x"></i>
-        <a class="clickable" href="'. site_url("/user/jcadmin"). '">JC Admin</a> <br />
-        Journal club admin</td>
-        <td></td>
-    </tr>';
-}
-
-
-$table .= '</table>';
-echo $table;
-
-echo '<h1>Booking</h1>';
-$html = '<table class="admin">';
-
 // Count pending requests.
 $user = whoAmI();
-$reqs = getTableEntries( 'bookmyvenue_requests', 'date'
-                    , "status='PENDING' AND created_by='$user' GROUP BY gid" 
-                );
-
+$reqs = getTableEntries( 'bookmyvenue_requests'
+    , 'date'
+    , "status='PENDING' AND created_by='$user' GROUP BY gid" 
+);
 $flag = '';
 if( count( $reqs ) > 0)
     $flag = count( $reqs ) . " requests.";
-
-
-$html .= '
-    <tr>
-    <td>
-        <i class="fa fa-hand-pointer-o fa-2x"></i>
-         <a class="clickable" href="'. site_url("/user/book/venue"). '">Book for small event</a>
-         <br />
-         E.g. Labmeets,meeting,interview etc.. No email will be sent to Academic community.
-        <br /> <br /> <i class="fa fa-pencil-square-o fa-1x"></i>
-        <a href="'. site_url("/user/show_private") . '"> Manage My Private Events ('. $flag .')</a> 
-    </td>
-    <td>
-        <i class="fa fa-comments fa-2x"></i>
-        <a class="clickable" href="'. site_url("/user/register_talk"). '">Book for talks/seminar etc. (Academic Events)</a>
-        <br />
-        E.g. talk, seminar, public lectures, pre-synopsis/thesis seminar; essentially any event 
-        for which academic community needs to be notified by email.
-        <br /> <br /> <i class="fa fa-pencil-square-o fa-1x"></i>
-        
-        <a href="'. site_url("/user/show_public"). '">Manage my public events.</a>
-    </td>
-   </tr>
-   </table>';
-echo $html;
-
-echo '<h1>Lab management</h1>';
-
-echo printNote( 
-    "You can create equipments private to your lab. These equipments can be booked by your lab members. 
-    This is not a replacement for common-equipment booking system." 
-    );
-
-$html = '<table class="admin">';
-$html .= '
-    <tr>
-    <td>
-        <i class="fa fa-hand-archive fa-2x"></i>
-         <a class="clickable" href="'. site_url("/user/inventory_browse"). '">Browse Lab Inventory or Book Equipments</a>
-    </td>
-    <td>
-        <i class="fa fa-flask fa-2x"></i>
-         <a class="clickable" href="'. site_url("/user/inventory_manage"). '">Manage Lab Inventory</a>
-    </td>
-    </tr>
-   </table>';
-echo $html;
-
-if( anyOfTheseRoles( 'ADMIN,BOOKMYVENUE_ADMIN,JOURNALCLUB_ADMIN,ACAD_ADMIN' ) )
-{
-   echo "<h1> <i class=\"fa fa-cogs\"></i>   Admin</h1>";
-   $roles =  getRoles(whoAmI() );
-
-   $html = "<table class=\"admin\">";
-   if( in_array( "ADMIN", $roles ) )
-       $html .= '<tr><td>
-                <i class="fa fa-lock fa-2x"></i>
-                <a class="clickable" href="'. site_url("/admin"). '"> Admin</a></td>';
-
-   if( in_array( "BOOKMYVENUE_ADMIN", $roles ) )
-       $html .= '<td><i class="fa fa-calendar-plus-o fa-2x"></i>
-            <a class="clickable" href="'. site_url("/adminbmv"). '"> 
-            BookMyVenue Admin</a></td>';
-   else
-       $html .= ' <td></td>';
-
-   if( in_array( "ACAD_ADMIN", $roles ) )
-       $html .= '<td><i class="fa fa-graduation-cap fa-2x"></i>
-            <a class="clickable" href="'. site_url("/adminacad"). '">Academic Admin</a></td>';
-   else
-       $html .= ' <td></td>';
-
-   $html .= "</tr></table>";
-   echo $html;
-}
-
-if(anyOfTheseRoles("SERVICES_ADMIN")) 
-{
-    echo "<h1> Services Admin </h1>";
-    $html = "<table class=\"admin\">";
-    $html .= '<tr>';
-    $html .= '<td>
-        <i class="fa fa-bus fa-2x"></i>
-        <a class="clickable" href="'.site_url( 'adminservices/transport') . '">Manage Transport</a>
-        </td>';
-    $html .= '
-    <td>
-        <i class="fa fa-cutlery fa-2x"></i>
-        <a class="clickable" href="'.site_url("adminservices/canteen").'">Manage Canteen Menu</a>
-    </td>';
-    $html .= '</tr>';
-    echo $html ;
-}
-
 ?>
+
+<div class="row">
+<div class="col card text-center m-3 p-1">
+     <a class="clickable fa fa-hand-pointer-o fa-2x" 
+         href="<?=site_url("/user/book/venue")?>">
+         Book for small event
+     </a>
+     <br />
+     E.g. Labmeets,meeting,interview etc.. No email will be sent to Academic community.
+    <br /> <br /> 
+    <a class="fa fa-pencil-square-o fa-1x" 
+        href="<?=site_url("/user/show_private")?>">
+        Manage My Private Events ('<?=$flag?>)
+    </a> 
+</div>
+<div class="col card text-center m-3 p-1">
+    <a class="fa fa-comments fa-2x clickable" 
+        href="<?=site_url("/user/register_talk")?>">
+        Book for talks/seminar etc. (Academic Events)
+    </a>
+    <br />
+    E.g. talk, seminar, public lectures, pre-synopsis/thesis seminar; essentially any event 
+    for which academic community needs to be notified by email.
+    <br /> <br />
+    <a class="fa fa-pencil-square-o fa-1x" 
+        href="<?=site_url("/user/show_public")?>">Manage my public events.
+    </a>
+</div>
+</div>
+
+<h1>Lab management</h1>
+
+<div class="text-left">
+You can create equipments private to your lab. These equipments can be 
+booked by your lab members.  This is not a replacement of 
+common equipment booking system.</div>
+
+<div class="row">
+<div class="col card text-center m-3 p-1">
+    <i class="fa fa-hand-archive fa-2x"></i>
+     <a class="clickable" href="'. site_url("/user/inventory_browse"). '">Browse Lab Inventory or Book Equipments</a>
+</div>
+<div class="col card text-center m-3 p-1">
+     <a class="clickable fa fa-flask fa-2x" 
+         href="<?=site_url("/user/inventory_manage")?>"
+        >Manage Lab Inventory</a>
+</div>
+</div>
+
+<?php
+$roles =  getRoles(whoAmI() ); 
+?>
+
+<?php if(anyOfTheseRoles('ADMIN,BOOKMYVENUE_ADMIN,JOURNALCLUB_ADMIN,ACAD_ADMIN')): ?>
+    <h1> <i class="fa fa-cogs"></i> Admin</h1>
+
+    <div class="row">
+    <?php if(in_array("ADMIN", $roles)): ?>
+       <div class="col card text-center m-3 p-1"> 
+            <a class="clickable fa fa-lock fa-2x" 
+                href="<?=site_url("/admin")?>"> Admin</a>
+        </div>
+    <?php endif; ?>
+
+    <?php if(in_array("BOOKMYVENUE_ADMIN", $roles)): ?>
+       <div class="col card text-center m-3 p-1">
+           <a class="clickable fa fa-calendar-plus-o fa-2x" 
+                href="<?=site_url("/adminbmv")?>"> BookMyVenue Admin</a>
+       </div>
+    <?php else: ?>
+       <div class="col card text-center m-3 p-1"></div>
+    <?php endif; ?>
+
+    <?php if( in_array( "ACAD_ADMIN", $roles)): ?>
+       <div class="col card text-center m-3 p-1">
+            <a class="clickable fa fa-graduation-cap fa-2x" 
+                href="<?=site_url("/adminacad")?>"> Academic Admin</a>
+       </div>
+    <?php else: ?>
+       <td></td>
+    <?php endif; ?>
+    </div>
+<?php endif; ?>
+
+<?php if(anyOfTheseRoles("SERVICES_ADMIN")): ?>
+    <h1> Services Admin </h1>
+    <div class="row">
+        <div class="col card text-center m-3 p-1">
+            <a class="clickable fa fa-bus fa-2x" 
+                href="<?=site_url( 'adminservices/transport')?>">Manage Transport</a>
+        </div>
+        <div class="col card text-center m-3 p-1">
+            <a class="clickable fa fa-cutlery fa-2x"
+            href="<?=site_url("adminservices/canteen")?>">Manage Canteen Menu</a>
+        </div>
+    </div>
+<?php endif; ?>
