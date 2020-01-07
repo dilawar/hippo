709d2c5cf6e87ac9cf148f4c5627100a1fdb6e33
Dilawar Singh <dilawars@ncbs.res.in>
2020-01-07 11:39:40 +0530

    Added missing file.

---
+++ application/views/admin_acad_aws_list.php
@@ -0,0 +1,44 @@
+<script>
+$( function() {
+    $( "#accordion" ).accordion({
+        collapsible: true
+        , heightStyle: "fill"
+        });
+    });
+</script>
+
+<?php
+require_once BASEPATH.'autoload.php';
+echo userHTML( );
+
+$awses = $AWSES_CI;
+
+// Create various maps from these AWSes.
+$awsByDate = [];
+foreach($awses as $aws)
+    $awsByDate[$aws['date']][] = $aws;
+?>
+
+
+
+<table class="table">
+<?php foreach($awsByDate as $date => $awses): ?> 
+    <tr>
+        <td> <?= humanReadableDate($date) ?> </td>
+        <?php foreach($awses as $aws): ?>
+        <td> <?= $aws['title'] ?> </td>
+        <?php endforeach; ?>
+    </tr>
+<?php endforeach;?>
+</table>
+
+
+<?= goBackToPageLink("Go back") ?>
+<script src="<?=base_url()?>./node_modules/xlsx/dist/xlsx.core.min.js"></script>
+<script src="<?=base_url()?>./node_modules/file-saverjs/FileSaver.min.js"></script>
+<script src="<?=base_url()?>./node_modules/tableexport/dist/js/tableexport.min.js"></script>
+
+<script type="text/javascript" charset="utf-8">
+TableExport(document.getElementsByClassName("exportable"));
+</script>
+
