e43dd389973e4f352270f306f38e02caf101271c
Dilawar Singh <dilawars@ncbs.res.in>
2020-05-22 16:34:13 +0530

    Fixed AWS email bug..

--- cron/aws_friday_notification.php
+++ cron/aws_friday_notification.php
@@ -22,13 +22,13 @@ function aws_friday_notification_cron()
         if ($res['speakers']) {
             $subject = 'Next week Annual Work Seminar (' . humanReadableDate($nextMonday) . ') by ';
             $subject .= implode(', ', $res['speakers']);
-            $mail = $res['email'];
+            $mail = $res['email_body'];
             $res = sendHTMLEmail($mail, $subject, $to, $cclist);
         } else {
             // There is no AWS this monday.
             $subject = 'No Annual Work Seminar next week (' . humanReadableDate($nextMonday) . ')';
 
-            $mail = $res['email'];
+            $mail = $res['email_body'];
             echo  "Sending to $to, $cclist with subject $subject";
             sendHTMLEmail($mail, $subject, $to, $cclist);
         }
