620eafcedb6435bdd54738637b90581323c68bed
Dilawar Singh <dilawars@ncbs.res.in>
2021-05-25 05:52:05 +0530

    minor tweaks.

--- application/config/config.php
+++ application/config/config.php
@@ -229,7 +229,7 @@ $config['allow_get_array'] = true;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 1;
+$config['log_threshold'] = [1, 3, 5];
 
 /*
 |--------------------------------------------------------------------------
